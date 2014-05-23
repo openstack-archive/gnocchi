8ea16ff2391213cdb2a3cda0fd16dc7dd22be31f
Julien Danjou <julien@danjou.info>
2014-05-23 11:02:21 +0200

    Add .gitreview file
    Change-Id: I418de2e0587114f9181fdda8f17518b2c43557a4

--- run-tests.sh
+++ run-tests.sh
@@ -15,7 +15,9 @@ PGSQL_DATA=`mktemp -d /tmp/gnocchi-psql-XXXXX`
 PGSQL_PATH=`pg_config --bindir`
 ${PGSQL_PATH}/initdb ${PGSQL_DATA}
 mkfifo ${PGSQL_DATA}/out
-${PGSQL_PATH}/postgres -F -k ${PGSQL_DATA} -D ${PGSQL_DATA} -p 9823 &> ${PGSQL_DATA}/out &
+${PGSQL_PATH}/postgres -F -k ${PGSQL_DATA} -D ${PGSQL_DATA} -p 9823 &
+sleep 30
+exit 1
 # Wait for PostgreSQL to start listening to connections
 wait_for_line "database system is ready to accept connections" ${PGSQL_DATA}/out
 export GNOCCHI_TEST_PGSQL_URL="postgresql:///?host=${PGSQL_DATA}&port=9823&dbname=template1"
