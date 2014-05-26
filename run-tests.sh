ec65274e091838af45ac4e3990d010a5c665ba81
Julien Danjou <julien@danjou.info>
2014-05-26 17:06:01 +0200

    Port to Python 3
    Change-Id: I3ec0fd1064aa4fb3467f9881b03ce6ba4bc64458

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 memcached &
+python -c 'import lockfile.linklockfile'
+python -m gnocchi.openstack.common.db.sqlalchemy.test_migrations
 
 python setup.py testr --slowest --testr-args="$*"
 
