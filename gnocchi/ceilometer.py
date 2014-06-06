b64e09f5d2d9a8ab34642ba5770cf1ae1a67fe81
Julien Danjou <julien@danjou.info>
2014-06-06 17:08:18 +0200

    Add a Ceilometer collector dispatcher
    This introduces a new dispatcher for Ceilometer that is able to
    transform and publisher the Ceilometer samples to Gnocchi.
    
    Change-Id: I424b991075ddc6ca7ef6bd7c6d17db247bf90c83

---
+++ gnocchi/ceilometer.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2014 eNovance
+#
+# Authors: Julien Danjou <julien@danjou.info>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import json
+
+import requests
+
+from ceilometer import dispatcher
+from oslo.config import cfg
+
+
+dispatcher_opts = [
+    cfg.StrOpt('gnocchi_url',
+               default="http://localhost:8041",
+               help='URL to Gnocchi.'),
+]
+
+cfg.CONF.register_opts(dispatcher_opts, group="dispatcher_gnocchi")
+
+
+class GnocchiDispatcher(dispatcher.Base):
+
+    def __init__(self, conf):
+        super(GnocchiDispatcher, self).__init__(conf)
+        self.gnocchi_url = conf.dispatcher_gnocchi.gnocchi_url
+
+    def record_metering_data(self, data):
+        if data['counter_name'] == 'instance':
+            resource_type = 'instance'
+            params = {
+                "host": data['metadata']['host'],
+                "flavor_id": data['metadata']['instance_type']['flavor_id'],
+                "image_ref": data['metadata']['image_ref'],
+                "display_name": data['metadata']['display_name'],
+                "architecture": data['metadata']['architecture'],
+            }
+        else:
+            resource_type = 'generic'
+            params = {}
+        r = requests.post("%s/v1/resource/%s"
+                          % (self.gnocchi_url, resource_type),
+                          data=json.dumps(params))
+        if r.status_code == 409:
+            # Update
+            pass
+        # TODO(jd) Post metrics to entities
+
+    @staticmethod
+    def record_events(events):
+        # TODO(jd) Write dat code.
+        pass
