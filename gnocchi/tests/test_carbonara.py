10d8ede5afdd9f38f42e3af506350cd8664ac1ac
Julien Danjou <julien@danjou.info>
2014-06-30 12:03:42 +0200

    carbonara: change time serie aggregation implementation
    Following recent discussions, this uses a new scheme to implement the
    timeserie aggregation in Carbonara.
    We use a infinite-precision timeserie in TimeSerieArchive that is used
    to compute the aggregation of all the archived timeseries.
    
    Change-Id: I346ba5b316049ba622c8f6e633cf9aaea1c0519c

--- gnocchi/tests/test_carbonara.py
+++ gnocchi/tests/test_carbonara.py
@@ -17,179 +17,259 @@
 # under the License.
 import datetime
 
+import pandas
 import testtools
 
 from gnocchi import carbonara
 
 
-class TestTimeSerie(testtools.TestCase):
+class TestBoundTimeSerie(testtools.TestCase):
+    def test_base(self):
+        carbonara.BoundTimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
+                                  datetime.datetime(2014, 1, 1, 12, 0, 4),
+                                  datetime.datetime(2014, 1, 1, 12, 0, 9)],
+                                 [3, 5, 6])
+
+    def test_timespan(self):
+        ts = carbonara.BoundTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 4),
+             datetime.datetime(2014, 1, 1, 12, 0, 9)],
+            [3, 5, 6],
+            timespan='5s')
+        self.assertEqual(len(ts), 2)
+        ts.set_values([(datetime.datetime(2014, 1, 1, 12, 0, 10), 3),
+                       (datetime.datetime(2014, 1, 1, 12, 0, 11), 4)])
+        self.assertEqual(len(ts), 3)
+
+    def test_timespan_timelimit(self):
+        ts = carbonara.BoundTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 4),
+             datetime.datetime(2014, 1, 1, 12, 0, 9)],
+            [3, 5, 6],
+            timespan='5s')
+        self.assertEqual(len(ts), 2)
+        ts.set_values([(datetime.datetime(2014, 1, 1, 12, 0, 10), 3),
+                       (datetime.datetime(2014, 1, 1, 12, 0, 11), 4)])
+        self.assertEqual(len(ts), 3)
+
+        self.assertRaises(
+            carbonara.NoDeloreanAvailable,
+            ts.set_values,
+            [(datetime.datetime(2014, 1, 1, 12, 0, 0), 42)],
+        )
+
+
+class TestAggregatedTimeSerie(testtools.TestCase):
 
     def test_base(self):
-        carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                             datetime.datetime(2014, 1, 1, 12, 0, 4),
-                             datetime.datetime(2014, 1, 1, 12, 0, 9)],
-                            [3, 5, 6])
+        carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 4),
+             datetime.datetime(2014, 1, 1, 12, 0, 9)],
+            [3, 5, 6])
 
     def test_different_length_in_timestamps_and_data(self):
         self.assertRaises(ValueError,
-                          carbonara.TimeSerie,
+                          carbonara.AggregatedTimeSerie,
                           [datetime.datetime(2014, 1, 1, 12, 0, 0),
                            datetime.datetime(2014, 1, 1, 12, 0, 4),
                            datetime.datetime(2014, 1, 1, 12, 0, 9)],
                           [3, 5])
 
     def test_max_size(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 0, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 0, 9)],
-                                 [3, 5, 6],
-                                 max_size=2)
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 4),
+             datetime.datetime(2014, 1, 1, 12, 0, 9)],
+            [3, 5, 6],
+            max_size=2)
         self.assertEqual(2, len(ts))
         self.assertEqual(ts[0], 5)
         self.assertEqual(ts[1], 6)
 
     def test_down_sampling(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 0, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 0, 9)],
-                                 [3, 5, 7],
-                                 sampling='5Min')
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 4),
+             datetime.datetime(2014, 1, 1, 12, 0, 9)],
+            [3, 5, 7],
+            sampling='5Min')
         self.assertEqual(1, len(ts))
         self.assertEqual(5, ts[datetime.datetime(2014, 1, 1, 12, 0, 0)])
 
     def test_down_sampling_with_max_size(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min',
-                                 max_size=2)
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 1, 4),
+             datetime.datetime(2014, 1, 1, 12, 1, 9),
+             datetime.datetime(2014, 1, 1, 12, 2, 12)],
+            [3, 5, 7, 1],
+            sampling='1Min',
+            max_size=2)
         self.assertEqual(2, len(ts))
         self.assertEqual(6, ts[datetime.datetime(2014, 1, 1, 12, 1, 0)])
         self.assertEqual(1, ts[datetime.datetime(2014, 1, 1, 12, 2, 0)])
 
     def test_down_sampling_with_max_size_and_method_max(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min',
-                                 max_size=2,
-                                 aggregation_method='max')
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 1, 4),
+             datetime.datetime(2014, 1, 1, 12, 1, 9),
+             datetime.datetime(2014, 1, 1, 12, 2, 12)],
+            [3, 5, 70, 1],
+            sampling='1Min',
+            max_size=2,
+            aggregation_method='max')
         self.assertEqual(2, len(ts))
-        self.assertEqual(7, ts[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(70, ts[datetime.datetime(2014, 1, 1, 12, 1, 0)])
         self.assertEqual(1, ts[datetime.datetime(2014, 1, 1, 12, 2, 0)])
 
     def test_to_dict_from_dict(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min',
-                                 max_size=2,
-                                 aggregation_method='max')
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 1, 4),
+             datetime.datetime(2014, 1, 1, 12, 1, 9),
+             datetime.datetime(2014, 1, 1, 12, 2, 12)],
+            [3, 5, 7, 1],
+            sampling='1Min',
+            max_size=2,
+            aggregation_method='max')
 
-        ts2 = carbonara.TimeSerie.from_dict(ts.to_dict())
+        ts2 = carbonara.AggregatedTimeSerie.from_dict(ts.to_dict())
         self.assertEqual(ts, ts2)
 
-    def test_update(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 3, 12)],
-                                 [3, 5, 7, 100],
-                                 sampling='1Min',
-                                 max_size=3)
-        ts2 = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 2, 0),
-                                   datetime.datetime(2014, 1, 1, 12, 3, 4),
-                                   datetime.datetime(2014, 1, 1, 12, 4, 9),
-                                   datetime.datetime(2014, 1, 1, 12, 5, 12)],
-                                  [8, 15, 27, 6])
-        ts.update(ts2)
-
-        self.assertEqual(3, len(ts))
-        self.assertEqual(57.5, ts[datetime.datetime(2014, 1, 1, 12, 3, 0)])
-        self.assertEqual(27, ts[datetime.datetime(2014, 1, 1, 12, 4, )])
-        self.assertEqual(6, ts[datetime.datetime(2014, 1, 1, 12, 5, )])
-
     def test_serialize(self):
-        ts = carbonara.TimeSerie(
+        ts = carbonara.AggregatedTimeSerie(
             [datetime.datetime(2014, 1, 1, 12, 0, 0),
              datetime.datetime(2014, 1, 1, 12, 1, 4),
              datetime.datetime(2014, 1, 1, 12, 2, 9),
              datetime.datetime(2014, 1, 1, 12, 3, 12)],
             [3, 5, 7, 100],
             sampling='1Min',
-            max_size=3)
+            block_size='1Min',
+            max_size=10)
         s = ts.serialize()
-        self.assertEqual(ts, carbonara.TimeSerie.unserialize(s))
+        self.assertEqual(ts, carbonara.AggregatedTimeSerie.unserialize(s))
+
+    def test_truncate_block_size(self):
+        ts = carbonara.AggregatedTimeSerie(
+            [datetime.datetime(2014, 1, 1, 12, 0, 0),
+             datetime.datetime(2014, 1, 1, 12, 0, 5),
+             datetime.datetime(2014, 1, 1, 12, 1, 4),
+             datetime.datetime(2014, 1, 1, 12, 1, 9),
+             datetime.datetime(2014, 1, 1, 12, 2, 12)],
+            [3, 8, 5, 7, 1],
+            max_size=5,
+            block_size=pandas.tseries.offsets.Minute(1))
+        self.assertEqual(5, len(ts))
+        ts.set_values([(datetime.datetime(2014, 1, 1, 12, 3, 19), 123)])
+        self.assertEqual(4, len(ts))
+        self.assertEqual(5, ts[datetime.datetime(2014, 1, 1, 12, 1, 4)])
+        self.assertEqual(7, ts[datetime.datetime(2014, 1, 1, 12, 1, 9)])
+        self.assertEqual(1, ts[datetime.datetime(2014, 1, 1, 12, 2, 12)])
+        self.assertEqual(123, ts[datetime.datetime(2014, 1, 1, 12, 3, 19)])
+        ts.set_values([(datetime.datetime(2014, 1, 1, 12, 3, 20), 124)])
+        self.assertEqual(5, len(ts))
 
 
-class TestTimeSerieCollection(testtools.TestCase):
+class TestTimeSerieArchive(testtools.TestCase):
 
     def test_fetch(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min')
-        ts2 = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                   datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                   datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                   datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                  [3, 5, 7, 1],
-                                  sampling='5Min')
+        tsc = carbonara.TimeSerieArchive.from_definitions(
+            [(pandas.tseries.offsets.Minute(1), 60),
+             (pandas.tseries.offsets.Minute(5), 24)])
 
-        tsc = carbonara.TimeSerieCollection([ts, ts2])
-        r = tsc[datetime.datetime(2014, 1, 1, 12, 0, 0)]
-        self.assertEqual(3, r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
-        self.assertEqual(6, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
-        self.assertEqual(1, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 0, 0), 3),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 4), 4),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 9), 7),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 1), 15),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 12), 1)])
 
-    def test_fetch_outside_low_precision(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min')
-        ts2 = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 3, 0),
-                                   datetime.datetime(2014, 1, 1, 12, 5, 4),
-                                   datetime.datetime(2014, 1, 1, 12, 8, 9),
-                                   datetime.datetime(2014, 1, 1, 12, 14, 12)],
-                                  [32, 25, 27, 21],
-                                  sampling='5Min')
-
-        tsc = carbonara.TimeSerieCollection([ts, ts2])
         r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
-        self.assertEqual(5, len(r))
+
         self.assertEqual(3, r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
-        self.assertEqual(6, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
-        self.assertEqual(1, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
-        self.assertEqual(26, r[datetime.datetime(2014, 1, 1, 12, 5, 0)])
-        self.assertEqual(21, r[datetime.datetime(2014, 1, 1, 12, 10, 0)])
-        self.assertRaises(KeyError, r.__getitem__,
-                          datetime.datetime(2014, 1, 1, 12, 6, 0))
+        self.assertEqual(5.5, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(8, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 2, 10), 11)])
+
+        r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
+
+        self.assertEqual(3, r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
+        self.assertEqual(5.5, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(9, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+
+    def test_fetch_agg_std(self):
+        tsc = carbonara.TimeSerieArchive.from_definitions(
+            [(pandas.tseries.offsets.Minute(1), 60),
+             (pandas.tseries.offsets.Minute(5), 24)],
+            aggregation_method='std')
+
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 0, 0), 3),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 4), 4),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 9), 7),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 1), 15),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 12), 1)])
+
+        r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
+
+        self.assertAlmostEqual(5.4772255750516612,
+                               r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
+        self.assertAlmostEqual(2.1213203435596424,
+                               r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(9.8994949366116654,
+                         r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 2, 10), 110)])
+
+        r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
+
+        self.assertEqual(42.739521132865619,
+                         r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
+        self.assertEqual(2.1213203435596424,
+                         r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(59.304300012730948,
+                         r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+
+    def test_fetch_agg_max(self):
+        tsc = carbonara.TimeSerieArchive.from_definitions(
+            [(pandas.tseries.offsets.Minute(1), 60),
+             (pandas.tseries.offsets.Minute(5), 24)],
+            aggregation_method='max')
+
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 0, 0), 3),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 4), 4),
+                        (datetime.datetime(2014, 1, 1, 12, 1, 9), 7),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 1), 15),
+                        (datetime.datetime(2014, 1, 1, 12, 2, 12), 1)])
+
+        r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
+
+        self.assertEqual(3, r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
+        self.assertEqual(7, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(15, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
+
+        tsc.set_values([(datetime.datetime(2014, 1, 1, 12, 2, 10), 110)])
+
+        r = tsc.fetch(datetime.datetime(2014, 1, 1, 12, 0, 0))
+
+        self.assertEqual(3, r[datetime.datetime(2014, 1, 1, 12, 0, 0)])
+        self.assertEqual(7, r[datetime.datetime(2014, 1, 1, 12, 1, 0)])
+        self.assertEqual(110, r[datetime.datetime(2014, 1, 1, 12, 2, 0)])
 
     def test_serialize(self):
-        ts = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                  datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                  datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                 [3, 5, 7, 1],
-                                 sampling='1Min')
-        ts2 = carbonara.TimeSerie([datetime.datetime(2014, 1, 1, 12, 0, 0),
-                                   datetime.datetime(2014, 1, 1, 12, 1, 4),
-                                   datetime.datetime(2014, 1, 1, 12, 1, 9),
-                                   datetime.datetime(2014, 1, 1, 12, 2, 12)],
-                                  [3, 5, 7, 1],
-                                  sampling='5Min')
+        tsc = carbonara.TimeSerieArchive.from_definitions(
+            [(pandas.tseries.offsets.Minute(1), None),
+             (pandas.tseries.offsets.Minute(5), None)])
+        tsc.set_values([
+            (datetime.datetime(2014, 1, 1, 12, 0, 0), 3),
+            (datetime.datetime(2014, 1, 1, 12, 1, 4), 5),
+            (datetime.datetime(2014, 1, 1, 12, 1, 9), 7),
+            (datetime.datetime(2014, 1, 1, 12, 2, 12), 1),
+        ])
 
-        tsc = carbonara.TimeSerieCollection([ts, ts2])
         self.assertEqual(tsc,
-                         carbonara.TimeSerieCollection.unserialize(
+                         carbonara.TimeSerieArchive.unserialize(
                              tsc.serialize()))
