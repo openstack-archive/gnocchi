5c35c380f665c15c6e12e457bcc9b1817ea5dc9a
Ana Malagon <atmalagon@gmail.com>
2014-07-02 22:03:19 -0400

    First try adding moving-averages to Gnocchi using InfluxDB
    Created a separate file for rolling statistics functions,
    first try at implementing a rolling mean in the
    get_measures API using InfluxDB as the storage backend.
    
    To get the moving-average in a query:
    
    GET /v1/entity/UUID/measures?aggregation=moving-average
    
    The window size is currently set to be the granularity specified
    upon entity creation.
    
    Change-Id: If40e58480f1512dd17b9c09cb75fd54c7193c257

--- gnocchi/storage/influx.py
+++ gnocchi/storage/influx.py
@@ -15,10 +15,12 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
 import collections
 import datetime
 import sys
+
+
+import pandas as pd
 
 from gnocchi.openstack.common import timeutils
 import logging
@@ -26,7 +28,9 @@ from oslo.config import cfg
 
 import influxdb
 
+from gnocchi import rolling_statistics
 from gnocchi import storage
+
 
 OPTIONS = [
     cfg.StrOpt('influx_host',
@@ -51,7 +55,7 @@ cfg.CONF.register_opts(OPTIONS, group='storage')
 LOG = logging.getLogger(__name__)
 
 Archive = collections.namedtuple('Archive', ['granularity', 'retention'])
-Format = collections.namedtuple('Fromat', ['timestamp', 'value'])
+Format = collections.namedtuple('Format', ['timestamp', 'value'])
 Point = collections.namedtuple('Point', ['timestamp', 'value'])
 
 
@@ -206,14 +210,15 @@ class InfluxStorage(storage.StorageDriver):
         :param aggregation: The type of aggregation to retrieve.
         :param granularity: The per-second granularity required.
         """
-
-        aggregation = InfluxStorage.NATIVE_AGGREGATES.get(aggregation, 'mean')
+        if aggregation != 'moving-average':
+            aggregation = InfluxStorage.NATIVE_AGGREGATES.get(aggregation,
+                                                              'mean')
 
         def _select(archive):
             params = dict(name='%s-data' % entity,
                           target='*', group_by='', limit='')
 
-            if archive.granularity > 1:
+            if archive.granularity > 1 and aggregation != 'moving-average':
                 params['target'] = '%s(value)' % aggregation
                 params['group_by'] = \
                     'group by time(%ds)' % archive.granularity
@@ -241,8 +246,14 @@ class InfluxStorage(storage.StorageDriver):
             return select % params
 
         def _format(archive):
-            value = 'value' if archive.granularity == 1 else aggregation
+            if archive.granularity == 1 or aggregation == 'moving-average':
+                value = 'value'
+            else:
+                value = aggregation
             return Format(timestamp='time', value=value)
+
+        def _as_string(timestamp):
+            return datetime.datetime.utcfromtimestamp(timestamp / 1000000.0)
 
         # TODO(eglynn): batch up per-archive queries
 
@@ -251,7 +262,6 @@ class InfluxStorage(storage.StorageDriver):
             query = _select(archive)
 
             data = self._query(query, entity)
-
             # data format returned by influx:
             #
             # unaggregated case:
@@ -264,21 +274,36 @@ class InfluxStorage(storage.StorageDriver):
             #     "columns": ["time", aggregate],
             #     "points":  [[epoch_seconds, value]]}]
 
+            # TODO(atmalagon): add query parameter for window size,
+            # generalize flag for 'rolling' aggregates.
+
             if data:
                 format = _format(archive)
                 ti = data[0]['columns'].index(format.timestamp)
                 vi = data[0]['columns'].index(format.value)
-
                 points.extend([Point(timestamp=p[ti], value=p[vi])
                                for p in data[0]['points']])
-
-        def _as_string(timestamp):
-            return datetime.datetime.utcfromtimestamp(timestamp / 1000000.0)
+            if data and aggregation == 'moving-average':
+                vals = [float(p.value) for p in points]
+                dates = [p.timestamp for p in points]
+                idx = pd.DatetimeIndex([_as_string(date) for date in dates])
+                ts = pd.Series(vals,index=idx)
+                ts = ts.sort_index()
+                wi = str(archive.granularity)+'s'
+                mavg = rolling_statistics.rolling_mean(ts,window=wi)
+                points = []
+                for i in range(len(mavg)):
+                    if mavg[i] == mavg[i]:
+                        #tests if not nan
+                        points.extend([Point(timestamp=mavg.index[i],
+                                             value=mavg[i])])
 
         # TODO(eglynn): returning a dict keyed by timestamp has two
         # unfortunate side-effects:
         #  * loses any datapoint ordering provided by the DB
         #  * finer-grain datapoints of the same timestamp mask out coarser-
         #    grain datapoints (unless granularity is explicitly selected)
-
-        return dict((_as_string(p.timestamp), p.value) for p in points)
+        if aggregation != 'moving-average':
+            return dict((_as_string(p.timestamp), p.value) for p in points)
+        else:
+            return dict((p.timestamp, p.value) for p in points)
