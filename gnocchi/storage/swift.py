dbca105df544381d9ff078e6349224bc8657e6a7
Julien Danjou <julien@danjou.info>
2014-08-11 17:43:32 +0200

    storage: implement archiving policy
    This is a first step towards unifying entities archive storage based on
    a template. The templates are for now hardcoded, but the next step will
    be to have them defined via the REST API.
    
    Change-Id: I0f277fba479b2f978ffddc5f788d5c4414468df9

--- gnocchi/storage/swift.py
+++ gnocchi/storage/swift.py
@@ -76,7 +76,7 @@ class SwiftStorage(storage.StorageDriver):
         self.aggregation_types = list(storage.AGGREGATION_TYPES)
         random.shuffle(self.aggregation_types)
 
-    def create_entity(self, entity, archive):
+    def create_entity(self, entity, archive_policy):
         # TODO(jd) A container per user in their account?
         resp = {}
         self.swift.put_container(entity, response_dict=resp)
@@ -90,7 +90,7 @@ class SwiftStorage(storage.StorageDriver):
             # may want to store it as its own object.
             tsc = carbonara.TimeSerieArchive.from_definitions(
                 [(pandas.tseries.offsets.Second(second), size)
-                 for second, size in archive],
+                 for second, size in storage.ARCHIVE_POLICIES[archive_policy]],
                 aggregation_method=aggregation)
             self.swift.put_object(entity, aggregation,
                                   tsc.serialize())
