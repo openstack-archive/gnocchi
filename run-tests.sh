e3151a6a907c3ffcdd22964ce01888711f63ce07
Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
2014-09-17 14:12:32 +0200

    Add a gnocchi dispatcher
    This introduces a new dispatcher for Gnocchi that is able to
    transform and publish the Ceilometer samples to Gnocchi.
    
    Co-Authored-by: Julien Danjou<julien@danjou.info>
    
    Change-Id: I424b991075ddc6ca7ef6bd7c6d17db247bf90c83

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -x
 memcached &
 
-python setup.py testr --slowest --testr-args="$*"
+[ "$1" = "--coverage" ] && COVERAGE=1
+
+ret=0
+if [ ! "$COVERAGE" ]; then
+   # Ceilometer dispatcher  tests
+   bash tools/init_testr_if_needed.sh
+   python setup.py testr --slowest --testr-args="--here=ceilometer_tests $*"
+   ret=$?
+fi
+
+[ "$ret" == "0" ] && python setup.py testr --slowest --testr-args="$*"
 
 ret=$?
 kill $(jobs -p)
