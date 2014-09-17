78bf9b9585edc4b28425f2eb8a7ae53824ef8d6a
Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
2014-09-17 23:59:58 +0200

    Add project resource
    This change adds the project resource with some entitites:
    
    storage.objects.incoming.bytes
    storage.objects.outgoing.bytes
    storage.api.request
    storage.objects.size
    storage.objects
    storage.objects.containers
    
    Change-Id: I77fa749df1f37e6705e7f943afc3aff1d51d0e6e

--- gnocchi/rest/__init__.py
+++ gnocchi/rest/__init__.py
@@ -284,6 +284,10 @@ class GenericResourceController(rest.RestController):
         pecan.response.status = 204
 
 
+class ProjectController(GenericResourceController):
+    _resource_type = 'project'
+
+
 class InstanceController(GenericResourceController):
     _resource_type = 'instance'
 
@@ -364,6 +368,11 @@ class GenericResourcesController(rest.RestController):
             pecan.abort(400, e)
 
 
+class ProjectsController(GenericResourcesController):
+    _resource_type = 'project'
+    _resource_rest_class = ProjectController
+
+
 class InstancesController(GenericResourcesController):
     _resource_type = 'instance'
     _resource_rest_class = InstanceController
@@ -379,6 +388,7 @@ class InstancesController(GenericResourcesController):
 class ResourcesController(rest.RestController):
     generic = GenericResourcesController()
     instance = InstancesController()
+    project = ProjectsController()
 
 
 class V1Controller(object):
