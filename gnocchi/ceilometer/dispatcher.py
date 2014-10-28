74eb6a3a412ce36ea0be278d10bb4968d79efac2
Ilya Tyaptin <ityaptin@mirantis.com>
2014-10-28 18:55:26 +0400

    [PoC] [WIP] Direct calls in dispatcher
    Used direct calls to indexer and storage instead api calls in dispacher.
    In case of Opentsdb driver it improves performance x10-x12 times. While using this change it turned out that current opentsdb driver change has kind of issues. So I'm debugging it.
    This change with direct sorage calls might be tested with swift backend.
    
    Change-Id: I654416d92ec9785ae7fffbe436dddbc23de112b6

--- gnocchi/ceilometer/dispatcher.py
+++ gnocchi/ceilometer/dispatcher.py
@@ -2,13 +2,13 @@
 # Copyright 2014 eNovance
 #
 # Authors: Julien Danjou <julien@danjou.info>
-#          Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
+# Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -20,6 +20,7 @@ from __future__ import absolute_import
 import itertools
 import json
 import operator
+import uuid
 
 from ceilometer import dispatcher
 from ceilometer.openstack.common.gettextutils import _
@@ -28,6 +29,12 @@ from oslo.config import cfg
 import requests
 import six
 import stevedore.dispatch
+
+from gnocchi import storage
+from gnocchi import indexer
+
+import pdb
+pdb.set_trace()
 
 LOG = log.getLogger(__name__)
 
@@ -38,10 +45,11 @@ dispatcher_opts = [
     cfg.StrOpt('archive_policy',
                default="low",
                help='The archive policy to use when the dispatcher '
-               'create a new entity.')
+                    'create a new entity.')
 ]
 
 cfg.CONF.register_opts(dispatcher_opts, group="dispatcher_gnocchi")
+cfg.CONF.import_group('storage', 'gnocchi.storage')
 
 CREATE_ENTITY = 'create_entity'
 CREATE_RESOURCE = 'create_resource'
@@ -73,6 +81,7 @@ def log_and_ignore_unexpected_workflow_error(func):
             func(*args, **kwargs)
         except UnexpectedWorkflowError as e:
             LOG.error(six.text_type(e))
+
     return log_and_ignore
 
 
@@ -82,11 +91,13 @@ class GnocchiDispatcher(dispatcher.Base):
         self.gnocchi_url = conf.dispatcher_gnocchi.url
         self.gnocchi_archive_policy = {
             'archive_policy':
-            cfg.CONF.dispatcher_gnocchi.archive_policy
+                cfg.CONF.dispatcher_gnocchi.archive_policy
         }
         self.mgmr = stevedore.dispatch.DispatchExtensionManager(
             'gnocchi.ceilometer.resource', lambda x: True,
             invoke_on_load=True)
+        self.storage = storage.get_driver(cfg.CONF)
+        self.indexer = indexer.get_driver(cfg.CONF)
 
     def record_metering_data(self, data):
         # FIXME(sileht): This method bulk the processing of samples
@@ -112,13 +123,13 @@ class GnocchiDispatcher(dispatcher.Base):
                             ext, resource_id, entity_name, list(samples),
                             resource_need_to_be_updated)
 
-                # FIXME(sileht): Does it reasonable to skip the resource
-                # update here ? Does differents kind of counter_name
-                # can have different metadata set ?
-                # (ie: one have only flavor_id, and an other one have only
-                # image_ref ?)
-                #
-                # resource_need_to_be_updated = False
+                        # FIXME(sileht): Does it reasonable to skip the resource
+                        # update here ? Does differents kind of counter_name
+                        # can have different metadata set ?
+                        # (ie: one have only flavor_id, and an other one have only
+                        # image_ref ?)
+                        #
+                        # resource_need_to_be_updated = False
 
     @log_and_ignore_unexpected_workflow_error
     def _process_samples(self, ext, resource_id, entity_name, samples,
@@ -132,26 +143,34 @@ class GnocchiDispatcher(dispatcher.Base):
             return
 
         resource_type = ext.name
-        measure_attributes = [{'timestamp': sample['timestamp'],
-                               'value': sample['counter_volume']}
-                              for sample in samples]
+
+        measure_attributes = [storage.Measure(
+            **{'timestamp': sample['timestamp'],
+               'value': sample['counter_volume']})
+            for sample in samples]
+        # TODO(ityaptin) Add measures verification
 
         try:
+            # storage.add_measures
             self._post_measure(resource_type, resource_id, entity_name,
                                measure_attributes)
         except NoSuchEntity:
             # NOTE(sileht): we try first to create the resource, because
             # they more chance that the resource doesn't exists than the entity
             # is missing, the should be reduce the number of resource API call
+            pdb.set_trace()
             resource_attributes = self._get_resource_attributes(
                 ext, resource_id, entity_name, samples)
             try:
+                # indexer.create_resource
                 self._create_resource(resource_type, resource_id,
                                       resource_attributes)
             except ResourceAlreadyExists:
                 try:
+                    # storage.create_entity(id, archive_policy definition)
+                    pdb.set_trace()
                     self._create_entity(resource_type, resource_id,
-                                        entity_name)
+                                        entity_name, resource_attributes)
                 except EntityAlreadyExists:
                     # NOTE(sileht): Just ignore the entity have been created in
                     # the meantime.
@@ -164,12 +183,15 @@ class GnocchiDispatcher(dispatcher.Base):
             # NOTE(sileht): we retry to post the measure but if it fail we
             # don't catch the exception to just log it and continue to process
             # other samples
+            # storage.post_measures
+            pdb.set_trace()
             self._post_measure(resource_type, resource_id, entity_name,
                                measure_attributes)
 
         if resource_need_to_be_updated:
             resource_attributes = self._get_resource_attributes(
                 ext, resource_id, entity_name, samples, for_update=True)
+            # update resource
             self._update_resource(resource_type, resource_id,
                                   resource_attributes)
 
@@ -191,95 +213,59 @@ class GnocchiDispatcher(dispatcher.Base):
 
     def _post_measure(self, resource_type, resource_id, entity_name,
                       measure_attributes):
-        r = requests.post("%s/v1/resource/%s/%s/entity/%s/measures"
-                          % (self.gnocchi_url, resource_type, resource_id,
-                             entity_name),
-                          headers={'Content-Type': "application/json"},
-                          data=json.dumps(measure_attributes))
-        if r.status_code == 404:
-            LOG.debug(_("The entity %(entity_name)s of "
-                        "resource %(resource_id)s doesn't exists"
-                        "%(status_code)d"),
-                      {'entity_name': entity_name,
-                       'resource_id': resource_id,
-                       'status_code': r.status_code})
+        try:
+            pdb.set_trace()
+            self.storage.add_measures(entity_name, measure_attributes)
+        except storage.EntityDoesNotExist:
             raise NoSuchEntity
-        elif int(r.status_code / 100) != 2:
-            raise UnexpectedWorkflowError(
-                _("Fail to post measure on entity %(entity_name)s of "
-                  "resource %(resource_id)s with status: "
-                  "%(status_code)d: %(msg)s") %
-                {'entity_name': entity_name,
-                 'resource_id': resource_id,
-                 'status_code': r.status_code,
-                 'msg': r.text})
+        except Exception as e:
+            raise UnexpectedWorkflowError(e.message)
         else:
             LOG.debug("Measure posted on entity %s of resource %s",
                       entity_name, resource_id)
 
     def _create_resource(self, resource_type, resource_id,
                          resource_attributes):
-        r = requests.post("%s/v1/resource/%s"
-                          % (self.gnocchi_url, resource_type),
-                          headers={'Content-Type': "application/json"},
-                          data=json.dumps(resource_attributes))
-        if r.status_code == 409:
-            LOG.debug("Resource %s already exists", resource_id)
+        try:
+            self.indexer.create_resource(resource_type, resource_id,
+                                         **resource_attributes)
+        except indexer.ResourceAlreadyExists:
             raise ResourceAlreadyExists
-
-        elif int(r.status_code / 100) != 2:
+        except Exception:
             raise UnexpectedWorkflowError(
-                _("Resource %(resource_id)s creation failed with "
-                  "status: %(status_code)d: %(msg)s") %
-                {'resource_id': resource_id,
-                 'status_code': r.status_code,
-                 'msg': r.text})
+                _("Resource %(resource_id)s creation failed with"))
         else:
             LOG.debug("Resource %s created", resource_id)
 
     def _update_resource(self, resource_type, resource_id,
                          resource_attributes):
-        r = requests.patch(
-            "%s/v1/resource/%s/%s"
-            % (self.gnocchi_url, resource_type, resource_id),
-            headers={'Content-Type': "application/json"},
-            data=json.dumps(resource_attributes))
-
-        if int(r.status_code / 100) != 2:
-            raise UnexpectedWorkflowError(
-                _("Resource %(resource_id)s update failed with "
-                  "status: %(status_code)d: %(msg)s") %
-                {'resource_id': resource_id,
-                 'status_code': r.status_code,
-                 'msg': r.text})
+        try:
+            self.indexer.update_resource(resource_type, resource_id,
+                                         **resource_attributes)
+        except indexer.NoSuchResource:
+            raise NoSuchResource
+        except indexer.UnknownResourceType:
+            raise NoSuchResource
+        except Exception as e:
+            raise UnexpectedWorkflowError(e.message)
         else:
             LOG.debug("Resource %s updated", resource_id)
 
-    def _create_entity(self, resource_type, resource_id, entity_name):
-        params = {entity_name: self.gnocchi_archive_policy}
-        r = requests.post("%s/v1/resource/%s/%s/entity"
-                          % (self.gnocchi_url, resource_type,
-                             resource_id),
-                          headers={'Content-Type': "application/json"},
-                          data=json.dumps(params))
-        if r.status_code == 409:
-            LOG.debug("Entity %s of resource %s already exists",
-                      entity_name, resource_id)
+    def _create_entity(self, resource_type, resource_id, entity_name,
+                       resource_attributes):
+        id = uuid.uuid4().hex
+        try:
+            del resource_attributes['entities']
+            self.indexer.create_resource('entity', id, **resource_attributes)
+            self.storage.create_entity(entity_name,
+                                       self.gnocchi_archive_policy)
+        except storage.EntityAlreadyExists:
             raise EntityAlreadyExists
-
-        elif int(r.status_code / 100) != 2:
-            raise UnexpectedWorkflowError(
-                _("Fail to create entity %(entity_name)s of "
-                  "resource %(resource_id)s with status: "
-                  "%(status_code)d: %(msg)s") %
-                {'entity_name': entity_name,
-                 'resource_id': resource_id,
-                 'status_code': r.status_code,
-                 'msg': r.text})
+        except Exception as e:
+            raise UnexpectedWorkflowError(e.message)
         else:
             LOG.debug("Entity %s of resource %s created",
                       entity_name, resource_id)
 
-    @staticmethod
-    def record_events(events):
+    def record_events(self, events):
         raise NotImplementedError
