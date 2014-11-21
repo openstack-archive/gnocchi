3ad78bd209320025901ffa2e55aa452d68ccf802
Julien Danjou <julien@danjou.info>
2014-11-21 10:30:21 +0100

    rest: add policy support for get entity
    Change-Id: I7f0d8130e442d160f8cd1e00aceea10d7839828e

--- gnocchi/tests/test_rest.py
+++ gnocchi/tests/test_rest.py
@@ -385,6 +385,15 @@ class EntityTest(RestTest):
         entity = json.loads(result.text)
         self.assertEqual(entity['archive_policy'], "medium")
 
+    def test_get_entity_with_another_user(self):
+        result = self.app.post_json("/v1/entity",
+                                    params={"archive_policy": "medium"},
+                                    status=201)
+        self.assertEqual("application/json", result.content_type)
+
+        with self.app.use_another_user():
+            self.app.get(result.headers['Location'], status=403)
+
     def test_get_detailed_entity(self):
         result = self.app.post_json(
             "/v1/entity",
