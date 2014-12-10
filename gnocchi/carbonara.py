c1ee4c44f01cf0576cb74ae236929f7ea93e7df3
Mehdi Abaakouk <sileht@sileht.net>
2014-12-10 08:33:32 +0100

    Aggregates across entities with missing datapoints
    Change-Id: Iae07c4b4fa4db995b2ee0ef27aa9fbc1c13121e5

--- gnocchi/carbonara.py
+++ gnocchi/carbonara.py
@@ -368,7 +368,10 @@ class TimeSerieArchive(object):
 
     @staticmethod
     def aggregated(timeseries, from_timestamp=None, to_timestamp=None,
-                   aggregation='mean'):
+                   aggregation='mean', needed_percent_of_overlap=None):
+        if needed_percent_of_overlap is None:
+            needed_percent_of_overlap = 100.0
+
         index = ['timestamp', 'granularity']
         columns = ['timestamp', 'granularity', 'value']
         dataframes = []
@@ -397,11 +400,11 @@ class TimeSerieArchive(object):
         maximum = len(points_count)
         minimum = len(points_count[points_count < len(timeseries)].dropna())
         percent_of_overlap = float(maximum - minimum) * 100.0 / float(maximum)
-        if percent_of_overlap < 100.0:
+        if percent_of_overlap < needed_percent_of_overlap:
             # TODO: allows a certain percent of missing datapoint
             raise UnAggregableTimeseries('Less than %f%% of datapoints '
                                          'overlap in this timespan (%.2f%%)' %
-                                         (100.0,
+                                         (needed_percent_of_overlap,
                                           percent_of_overlap))
 
         grouped = pandas.concat(dataframes).groupby(level=index)
