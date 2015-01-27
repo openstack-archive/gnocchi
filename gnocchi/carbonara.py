5b49683acf80560aad1ddd9d7cfece57e520a23c
Julien Danjou <julien@danjou.info>
2015-01-27 17:28:47 +0100

    carbonara: clean up useless code
    This remove useless code in various places.
    
    Change-Id: I51409701d6b9325da19717714d8a006537c4df33

--- gnocchi/carbonara.py
+++ gnocchi/carbonara.py
@@ -48,17 +48,13 @@ class UnAggregableTimeseries(Exception):
 class TimeSerie(object):
 
     def __init__(self, timestamps=None, values=None):
-        self.ts = pandas.Series(values, timestamps)
-        self.ts = self.ts.sort_index()
+        self.ts = pandas.Series(values, timestamps).sort_index()
 
     def __eq__(self, other):
         return (isinstance(other, TimeSerie)
                 and self.ts.all() == other.ts.all())
 
     def __getitem__(self, key):
-        return self.ts[key]
-
-    def get(self, key):
         return self.ts[key]
 
     def set_values(self, values):
@@ -96,13 +92,6 @@ class TimeSerie(object):
                            for timestamp, v
                            in six.iteritems(self.ts[~self.ts.isnull()])),
         }
-
-    def serialize(self):
-        return msgpack.dumps(self.to_dict())
-
-    @classmethod
-    def unserialize(cls, data):
-        return cls.from_dict(msgpack.loads(data, encoding='utf-8'))
 
     @staticmethod
     def _serialize_time_period(value):
@@ -213,9 +202,8 @@ class AggregatedTimeSerie(TimeSerie):
                 and self.aggregation_method == other.aggregation_method)
 
     def set_values(self, values):
-        values = list(values)
         super(AggregatedTimeSerie, self).set_values(values)
-        self._resample(min(list(values), key=operator.itemgetter(0))[0])
+        self._resample(min(values, key=operator.itemgetter(0))[0])
         self._truncate()
 
     @classmethod
