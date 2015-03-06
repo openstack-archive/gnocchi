69609427a397edba0aa40af3d4c46b99d9fb2c4a
Julien Danjou <julien@danjou.info>
2015-03-06 15:16:12 +0100

    tests: make authtoken less verbose
    Change-Id: If63310d1628b5d059d6a5ac2b26eea22b24352ae

--- gnocchi/tests/test_rest.py
+++ gnocchi/tests/test_rest.py
@@ -161,6 +161,14 @@ class RestTest(tests_base.TestCase):
                              group="keystone_authtoken")
         self.conf.set_override("cache", TestingApp.CACHE_NAME,
                                group='keystone_authtoken')
+        # TODO(jd) Override these options with values. They are not used, but
+        # if they are None (their defaults), the keystone authtoken middleware
+        # prints a warning… :( When the bug is fixed we can remove that!
+        # See https://bugs.launchpad.net/keystonemiddleware/+bug/1429179
+        self.conf.set_override("identity_uri", "foobar",
+                               group="keystone_authtoken")
+        self.conf.set_override("auth_uri", "foobar",
+                               group="keystone_authtoken")
         self.conf.import_opt("middlewares", "gnocchi.rest.app", group="api")
         if hasattr(self, "middlewares"):
             self.conf.set_override("middlewares",
