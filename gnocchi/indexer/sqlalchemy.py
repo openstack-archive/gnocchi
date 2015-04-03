247d974e9c285c1758df0df7e5ef5c3e13bfcf5c
Julien Danjou <julien@danjou.info>
2015-04-03 14:44:32 +0200

    sqlalchemy: use RDBMS check constraint where available
    We replace our Python check by a check in the DB for != MySQL.
    
    This requires change I7e36ef4385b227d1e62e18277d470047a369a238 in
    oslo.db to be released.
    
    Change-Id: I6027ac0159dddc3e8bf50a6a86e1d50f325e9d6d

--- gnocchi/indexer/sqlalchemy.py
+++ gnocchi/indexer/sqlalchemy.py
@@ -193,43 +193,50 @@ class SQLAlchemyIndexer(indexer.IndexerDriver):
                         **kwargs):
         resource_cls = self._resource_type_to_class(resource_type)
         session = self.engine_facade.get_session()
-        with session.begin():
-            q = session.query(
-                resource_cls).filter(
-                    resource_cls.id == resource_id)
-            # NOTE(jd) Always load metrics. It's sad, but since anyway what we
-            # return will end up on the wire, the user wants full info, so
-            # don't bother providing a with_metrics parameter.
-            q = q.options(sqlalchemy.orm.joinedload(resource_cls.metrics))
-            r = q.first()
-            if r is None:
-                raise indexer.NoSuchResource(resource_id)
+        try:
+            with session.begin():
+                q = session.query(
+                    resource_cls).filter(
+                        resource_cls.id == resource_id)
+                # NOTE(jd) Always load metrics. It's sad, but since anyway what
+                # we return will end up on the wire, the user wants full info,
+                # so don't bother providing a with_metrics parameter.
+                q = q.options(sqlalchemy.orm.joinedload(resource_cls.metrics))
+                r = q.first()
+                if r is None:
+                    raise indexer.NoSuchResource(resource_id)
 
-            if ended_at is not _marker:
-                # NOTE(jd) Could be better to have check in the db for that so
-                # we can just run the UPDATE
-                if r.started_at is not None and ended_at is not None:
-                    # Convert to UTC because we store in UTC :(
-                    ended_at = timeutils.normalize_time(ended_at)
-                    if r.started_at > ended_at:
-                        raise ValueError(
-                            "Start timestamp cannot be after end timestamp")
-                r.ended_at = ended_at
+                if ended_at is not _marker:
+                    # NOTE(jd) MySQL does not honor checks. I hate it.
+                    engine = self.engine_facade.get_engine()
+                    if engine.dialect.name == "mysql":
+                        if r.started_at is not None and ended_at is not None:
+                            # Convert to UTC because we store in UTC :(
+                            ended_at = timeutils.normalize_time(ended_at)
+                            if r.started_at > ended_at:
+                                raise indexer.ResourceValueError(
+                                    resource_type, "ended_at", ended_at)
+                    r.ended_at = ended_at
 
-            if kwargs:
-                for attribute, value in six.iteritems(kwargs):
-                    if hasattr(r, attribute):
-                        setattr(r, attribute, value)
-                    else:
-                        raise indexer.ResourceAttributeError(
-                            r.type, attribute)
+                if kwargs:
+                    for attribute, value in six.iteritems(kwargs):
+                        if hasattr(r, attribute):
+                            setattr(r, attribute, value)
+                        else:
+                            raise indexer.ResourceAttributeError(
+                                r.type, attribute)
 
-            if metrics is not _marker:
-                if not append_metrics:
-                    session.query(Metric).filter(
-                        Metric.resource_id == resource_id).update(
-                            {"resource_id": None})
-                self._set_metrics_for_resource(session, r, metrics)
+                if metrics is not _marker:
+                    if not append_metrics:
+                        session.query(Metric).filter(
+                            Metric.resource_id == resource_id).update(
+                                {"resource_id": None})
+                    self._set_metrics_for_resource(session, r, metrics)
+        except exception.DBConstraintError as e:
+            if e.check_name == "ck_started_before_ended":
+                raise indexer.ResourceValueError(
+                    resource_type, "ended_at", ended_at)
+            raise
 
         if metrics is not _marker:
             # NOTE(jd) Force reload of metrics – do it outside the session!
