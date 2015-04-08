3a9a23d08c83d2dff095a15705c2fb24da48cbe9
Julien Danjou <julien@danjou.info>
2015-04-08 16:54:49 +0200

    sqlalchemy: use precise DATETIME rather than DECIMAL in MySQL
    Recent MySQL versions have the support for precise timestamps. It's not
    on by default, but it's enable so we use a special type for that.
    
    Change-Id: I6717ab793d0b105edbca1054d80f17be6e3b4286

--- gnocchi/indexer/sqlalchemy_base.py
+++ gnocchi/indexer/sqlalchemy_base.py
@@ -2,8 +2,6 @@
 #
 # Copyright © 2014-2015 eNovance
 #
-# Authors: Julien Danjou <julien@danjou.info>
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -16,15 +14,18 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 from __future__ import absolute_import
-import calendar
+
 import datetime
-import decimal
 
 from oslo_db.sqlalchemy import models
+<<<<<<< HEAD
 from oslo_utils import timeutils
 from oslo_utils import units
+=======
+>>>>>>> sqlalchemy: use precise DATETIME rather than DECIMAL in MySQL
 import six
 import sqlalchemy
+from sqlalchemy.dialects import mysql
 from sqlalchemy.ext import declarative
 from sqlalchemy import types
 import sqlalchemy_utils
@@ -44,49 +45,10 @@ class PreciseTimestamp(types.TypeDecorator):
 
     impl = sqlalchemy.DateTime
 
-    @staticmethod
-    def _decimal_to_dt(dec):
-        """Return a datetime from Decimal unixtime format."""
-        if dec is None:
-            return None
-
-        integer = int(dec)
-        micro = (dec - decimal.Decimal(integer)) * decimal.Decimal(units.M)
-        daittyme = datetime.datetime.utcfromtimestamp(integer)
-        return daittyme.replace(microsecond=int(round(micro)))
-
-    @staticmethod
-    def _dt_to_decimal(utc):
-        """Datetime to Decimal.
-
-        Some databases don't store microseconds in datetime
-        so we always store as Decimal unixtime.
-        """
-        if utc is None:
-            return None
-
-        decimal.getcontext().prec = 30
-        return (decimal.Decimal(str(calendar.timegm(utc.utctimetuple()))) +
-                (decimal.Decimal(str(utc.microsecond)) /
-                 decimal.Decimal("1000000.0")))
-
     def load_dialect_impl(self, dialect):
         if dialect.name == 'mysql':
-            return dialect.type_descriptor(
-                types.DECIMAL(precision=20,
-                              scale=6,
-                              asdecimal=True))
+            return dialect.type_descriptor(mysql.DATETIME(fsp=6))
         return self.impl
-
-    def process_bind_param(self, value, dialect):
-        if dialect.name == 'mysql':
-            return self._dt_to_decimal(value)
-        return value
-
-    def process_result_value(self, value, dialect):
-        if dialect.name == 'mysql':
-            return self._decimal_to_dt(value)
-        return value
 
 
 class GnocchiBase(models.ModelBase):
@@ -208,12 +170,6 @@ class ResourceMixin(ResourceJsonifier):
     created_by_project_id = sqlalchemy.Column(
         sqlalchemy_utils.UUIDType(binary=False))
     started_at = sqlalchemy.Column(PreciseTimestamp, nullable=False,
-                                   # NOTE(jd): We would like to use
-                                   # sqlalchemy.func.now, but we can't
-                                   # because the type of PreciseTimestamp in
-                                   # MySQL is not a Timestamp, so it would
-                                   # not store a timestamp but a date as an
-                                   # integer.
                                    default=datetime.datetime.utcnow)
     revision_start = sqlalchemy.Column(PreciseTimestamp, nullable=False,
                                        default=timeutils.utcnow)
