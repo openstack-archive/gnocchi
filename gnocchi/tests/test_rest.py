ed92976877c9febce44a0d4662ef6f03ec5af837
Chris Dent <chdent@redhat.com>
2015-06-18 09:40:03 +0000

    Make test_rest keystone cache work with keystonemiddleware 2.0
    Token cache keys in keystonemiddleware 2.0 are sha256 hashes of the
    token key. This is _not_ the case in keystonemiddleware <2.0. This
    change adjusts the FakeMemcache to support both types of key.
    
    Change-Id: I28b5dbfbc915d424c74993732ddc041e528903e6

--- gnocchi/tests/test_rest.py
+++ gnocchi/tests/test_rest.py
@@ -44,14 +44,17 @@ load_tests = testscenarios.load_tests_apply_scenarios
 
 class FakeMemcache(object):
     VALID_TOKEN_ADMIN = '4562138218392830'
+    ADMIN_TOKEN_HASH = hashlib.sha256(VALID_TOKEN_ADMIN).hexdigest()
     USER_ID_ADMIN = str(uuid.uuid4())
     PROJECT_ID_ADMIN = str(uuid.uuid4())
 
     VALID_TOKEN = '4562138218392831'
+    TOKEN_HASH = hashlib.sha256(VALID_TOKEN).hexdigest()
     USER_ID = str(uuid.uuid4())
     PROJECT_ID = str(uuid.uuid4())
 
     VALID_TOKEN_2 = '4562138218392832'
+    TOKEN_2_HASH = hashlib.sha256(VALID_TOKEN_2).hexdigest()
     # We replace "-" to simulate a middleware that would send UUID in a non
     # normalized format.
     USER_ID_2 = str(uuid.uuid4()).replace("-", "")
@@ -59,7 +62,8 @@ class FakeMemcache(object):
 
     def get(self, key):
         dt = "2100-01-01T23:59:59"
-        if key == "tokens/%s" % self.VALID_TOKEN_ADMIN:
+        if (key == "tokens/%s" % self.ADMIN_TOKEN_HASH or
+                key == "tokens/%s" % self.VALID_TOKEN_ADMIN):
             return json.dumps(({'access': {
                 'token': {'id': self.VALID_TOKEN_ADMIN,
                           'expires': dt},
@@ -72,7 +76,8 @@ class FakeMemcache(object):
                         {'name': 'admin'},
                     ]},
             }}, dt))
-        elif key == "tokens/%s" % self.VALID_TOKEN:
+        elif (key == "tokens/%s" % self.TOKEN_HASH or
+                key == "tokens/%s" % self.VALID_TOKEN):
             return json.dumps(({'access': {
                 'token': {'id': self.VALID_TOKEN,
                           'expires': dt},
@@ -85,7 +90,8 @@ class FakeMemcache(object):
                         {'name': 'member'},
                     ]},
             }}, dt))
-        elif key == "tokens/%s" % self.VALID_TOKEN_2:
+        elif (key == "tokens/%s" % self.TOKEN_2_HASH or
+              key == "tokens/%s" % self.VALID_TOKEN_2):
             return json.dumps(({'access': {
                 'token': {'id': self.VALID_TOKEN_2,
                           'expires': dt},
