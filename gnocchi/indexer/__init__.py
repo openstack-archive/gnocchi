0dc7be97ee22c563aa67387b551b14dc2b3e056e
Mehdi Abaakouk <sileht@redhat.com>
2015-08-05 09:18:17 +0200

    Implements list_resources limit/ordering
    This change implements limit and ordering
    for the listing of resource.
    
    It also adds a [api]/max_limit to limit the number of
    elements returned, to avoid too many memory consumption
    on the server side.
    
    Partial bug: #1479830
    
    Change-Id: I9f4a7557809b04934adcef268f45b0fbb4ec0826

--- gnocchi/indexer/__init__.py
+++ gnocchi/indexer/__init__.py
@@ -206,6 +206,14 @@ class QueryAttributeError(QueryError, ResourceAttributeError):
         ResourceAttributeError.__init__(self, resource, attribute)
 
 
+class InvalidPagination(IndexerException):
+    """Error raised when a resource does not exist."""
+    def __init__(self, reason):
+        self.reason = reason
+        super(InvalidPagination, self).__init__(
+            "Invalid pagination: `%s'" % reason)
+
+
 class IndexerDriver(object):
     @staticmethod
     def __init__(conf):
@@ -237,7 +245,11 @@ class IndexerDriver(object):
     def list_resources(resource_type='generic',
                        attribute_filter=None,
                        details=False,
-                       history=False):
+                       history=False,
+                       limit=None,
+                       marker=None,
+                       sort_keys=None,
+                       sort_dirs=None):
         raise exceptions.NotImplementedError
 
     @staticmethod
