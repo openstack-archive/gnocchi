f4aaaf5096b91f7832f894aefe2cade97e98b90b
Mehdi Abaakouk <sileht@redhat.com>
2015-10-15 10:36:07 +0200

    TEST
    Change-Id: I34ca14fdf2db6c1c85df508cf02715e0f98b5b33

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -384,6 +384,10 @@ function start_gnocchi {
     if is_service_enabled gnocchi-api; then
         echo "Waiting for gnocchi-api to start..."
         if ! timeout $SERVICE_TIMEOUT sh -c "while ! curl --noproxy '*' -s $(gnocchi_service_url)/v1/resource/generic >/dev/null; do sleep 1; done"; then
+            sudo netstat -nlapute
+            curl -v --noproxy '*' -s $(gnocchi_service_url)/v1 2>&1 || true
+            curl -v --noproxy '*' -s $(gnocchi_service_url)/v1/resource 2>&1 || true
+            #curl -v --noproxy '*' -s $(gnocchi_service_url)/v1/resource/generic 2>&1 || true
             die $LINENO "gnocchi-api did not start"
         fi
     fi
