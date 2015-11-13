df421404cc7717657df833dcfdc95d24b210bfad
Pradeep Kilambi <pkilambi@redhat.com>
2015-11-13 16:33:27 -0500

    Add host and port config opts to statsd
    Change-Id: Icfbf9e01b4cb774a7af458b979f1be3938763560

--- gnocchi/statsd.py
+++ gnocchi/statsd.py
@@ -32,6 +32,12 @@ from gnocchi import utils
 LOG = log.getLogger(__name__)
 
 OPTS = [
+    cfg.StrOpt('host',
+               default='0.0.0.0',
+               help='The listen IP for statsd'),
+    cfg.IntOpt('port',
+               default=8125,
+               help='The port for statsd'),
     cfg.IntOpt('flush_delay',
                default=5,
                help='flush delay in seconds'),
@@ -184,8 +190,8 @@ def start():
     loop = asyncio.get_event_loop()
     # TODO(jd) Add TCP support
     listen = loop.create_datagram_endpoint(
-        # TODO(jd) Add config options for host/port
-        lambda: StatsdServer(stats), local_addr=('0.0.0.0', 8125))
+        lambda: StatsdServer(stats),
+        local_addr=(conf.statsd.host, conf.statsd.port))
     loop.call_later(conf.statsd.flush_delay, stats.flush)
     transport, protocol = loop.run_until_complete(listen)
 
