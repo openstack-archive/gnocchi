6ef6fc503c96fc2f29451dfcb6f21932a85f2c7c
heha <zhanghanqun@unitedstack.com>
2015-12-20 07:40:28 -0500

    Resource list filtered by project_id or created_by user_id and project_id
    List some resources filtered by the project_id that you provide, and
    the resources that you have created.
    
    Change-Id: I30f473ea720835447da3282647d803305390941a
    Closes-Bug: #1520439
    Closes-Bug: #1521895

--- gnocchi/rest/__init__.py
+++ gnocchi/rest/__init__.py
@@ -974,8 +974,10 @@ class GenericResourcesController(rest.RestController):
                 "resource_type": self._resource_type,
             })
             user, project = get_user_and_project()
-            attr_filter = {"and": [{"=": {"created_by_user_id": user}},
-                                   {"=": {"created_by_project_id": project}}]}
+            attr_filter = {"or": [
+                {"=": {"project_id": project}},
+                {"=": {"created_by_project_id": project}},
+            ]}
         else:
             attr_filter = None
 
@@ -1133,16 +1135,17 @@ class SearchResourceTypeController(rest.RestController):
                 "resource_type": self._resource_type,
             })
             user, project = get_user_and_project()
+            limited_attr_filter = {"or": [
+                {"=": {"project_id": project}},
+                {"=": {"created_by_project_id": project}},
+            ]}
             if attr_filter:
                 attr_filter = {"and": [
-                    {"=": {"created_by_user_id": user}},
-                    {"=": {"created_by_project_id": project}},
-                    attr_filter]}
-            else:
-                attr_filter = {"and": [
-                    {"=": {"created_by_user_id": user}},
-                    {"=": {"created_by_project_id": project}},
+                    limited_attr_filter,
+                    attr_filter,
                 ]}
+            else:
+                attr_filter = limited_attr_filter
 
         try:
             return pecan.request.indexer.list_resources(
