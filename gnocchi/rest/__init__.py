34f99e7bb4dc47733210bd1b962ef0c99b656bd1
Mehdi Abaakouk <sileht@redhat.com>
2016-01-18 11:58:08 +0100

    Bypass the auth when listing Gnocchi versions
    Change-Id: Ied303615d53ac8283ac815e84a90857d39edfcbf
    Closes-bug: #1522674

--- gnocchi/rest/__init__.py
+++ gnocchi/rest/__init__.py
@@ -320,7 +320,7 @@ class ArchivePoliciesController(rest.RestController):
         except indexer.ArchivePolicyAlreadyExists as e:
             abort(409, e)
 
-        location = "/v1/archive_policy/" + ap.name
+        location = "/archive_policy/" + ap.name
         set_resp_location_hdr(location)
         pecan.response.status = 201
         return ap
@@ -351,7 +351,7 @@ class ArchivePolicyRulesController(rest.RestController):
         except indexer.ArchivePolicyRuleAlreadyExists as e:
             abort(409, e)
 
-        location = "/v1/archive_policy_rule/" + ap.name
+        location = "/archive_policy_rule/" + ap.name
         set_resp_location_hdr(location)
         pecan.response.status = 201
         return ap
@@ -655,7 +655,7 @@ class MetricsController(rest.RestController):
                 archive_policy_name=body['archive_policy_name'])
         except indexer.NoSuchArchivePolicy as e:
             abort(400, e)
-        set_resp_location_hdr("/v1/metric/" + str(m.id))
+        set_resp_location_hdr("/metric/" + str(m.id))
         pecan.response.status = 201
         return m
 
@@ -977,7 +977,7 @@ class ResourcesController(rest.RestController):
             abort(400, e)
         except indexer.ResourceAlreadyExists as e:
             abort(409, e)
-        set_resp_location_hdr("/v1/resource/"
+        set_resp_location_hdr("/resource/"
                               + self._resource_type + "/"
                               + six.text_type(resource.id))
         etag_set_headers(resource)
@@ -1013,7 +1013,7 @@ class ResourcesByTypeController(rest.RestController):
     def get_all(self):
         return dict(
             (ext.name,
-             pecan.request.application_url + '/v1/resource/' + ext.name)
+             pecan.request.application_url + '/resource/' + ext.name)
             for ext in RESOURCE_SCHEMA_MANAGER)
 
     @pecan.expose()
@@ -1309,18 +1309,16 @@ class V1Controller(object):
             "version": "1.0",
             "links": [
                 {"rel": "self",
-                 "href": pecan.request.application_url + "/v1"}
+                 "href": pecan.request.application_url}
             ] + [
                 {"rel": name,
-                 "href": pecan.request.application_url + "/v1/" + name}
+                 "href": pecan.request.application_url + "/" + name}
                 for name in sorted(self.sub_controllers)
             ]
         }
 
 
-class RootController(object):
-    v1 = V1Controller()
-
+class VersionsController(object):
     @staticmethod
     @pecan.expose('json')
     def index():
