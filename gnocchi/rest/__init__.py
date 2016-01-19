bc70a274abd8b7e575e4cd9412c5b636b6124e02
Mehdi Abaakouk <sileht@redhat.com>
2016-01-19 22:55:59 +0100

    Implements resource attribute string
    This change allows to create resource attributes
    of type string.
    
    The choisen solution is that the indexer is responsible
    to provide voluptuous schemas and build resource columns
    according the the voluptuous schema that it propose.
    
    Another alternative could be that rest API provides
    jsonschema for each times and indexer is only
    responsible for the schema to sql column convertion.
    
    Partial implements: blueprint resource-type-rest-api
    Change-Id: I7877b6ea97dc70f3629e63abe5ef1ddf61d200b3

--- gnocchi/rest/__init__.py
+++ gnocchi/rest/__init__.py
@@ -789,9 +789,12 @@ class ResourceTypesController(rest.RestController):
 
     @staticmethod
     def ResourceTypeSchema(definition):
-        # FIXME(sileht): Add resource type attributes from the indexer
+        schemas = pecan.request.indexer.get_resource_attributes_schemas()
         return voluptuous.Schema({
             "name": six.text_type,
+            "attributes": {
+                six.text_type: voluptuous.Any(*tuple(schemas))
+            }
         })(definition)
 
     @pecan.expose()
@@ -954,8 +957,8 @@ def schema_for(resource_type):
         # TODO(sileht): Remove this legacy resource schema loading
         return RESOURCE_SCHEMA_MANAGER[resource_type].plugin
     else:
-        # TODO(sileht): Load schema from indexer
-        return GenericSchema
+        return ResourceSchema(pecan.request.indexer.get_resource_type(
+            resource_type).resource_schema())
 
 
 class ResourcesController(rest.RestController):
