37c8bfef357742e8df6a65cfe7a34b37dff23507
Mehdi Abaakouk <sileht@redhat.com>
2016-01-19 22:55:59 +0100

    Implements resource attribute string
    This change allows to create resource attributes
    of type string.
    
    The choisen solution is that the indexer is responsible
    to provide voluptuous schemas and build resource columns
    according the the voluptuous schema that it propose.
    
    Another alternative could be that rest API provides
    jsonschema for each times and indexer is only
    responsible for the schema to sql column convertion.
    
    Partial implements: blueprint resource-type-rest-api
    Change-Id: I7877b6ea97dc70f3629e63abe5ef1ddf61d200b3

---
+++ gnocchi/resource_type.py
@@ -0,0 +1,142 @@
+# -*- encoding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import six
+import stevedore
+import voluptuous
+
+
+INVALID_NAMES = [
+    "id", "type", "metrics",
+    "revision", "revision_start", "revision_end",
+    "started_at", "ended_at",
+    "user_id", "project_id",
+    "created_by_user_id", "created_by_project_id", "get_metric"
+]
+
+
+class InvalidResourceAttributeName(Exception):
+    """Error raised when the resource attribute name is invalid."""
+    def __init__(self, name):
+        super(InvalidResourceAttributeName, self).__init__(
+            "Resource attribute name %s is invalid" % str(name))
+        self.name = name
+
+
+class CommonAttributeSchema(object):
+    meta_schema_ext = {}
+    schema_ext = None
+
+    def __init__(self, type, name, required):
+        # TODO(sileht): Perhaps we need to be more restrictive ?
+        # max len ? only ascii char ? what does sql backends support ?
+        if name.startswith("_") or name in INVALID_NAMES:
+            raise InvalidResourceAttributeName(name)
+
+        self.name = name
+        self.required = required
+
+    @classmethod
+    def meta_schema(cls):
+        d = {
+            voluptuous.Required('type'): cls.typename,
+            voluptuous.Required('required', default=True): bool
+        }
+        d.update(cls.meta_schema_ext)
+        return d
+
+    def schema(self):
+        if self.required:
+            return {self.name: self.schema_ext}
+        else:
+            return {voluptuous.Optional(self.name): self.schema_ext}
+
+    def jsonify(self):
+        return {"type": self.typename,
+                "required": self.required}
+
+
+class StringSchema(CommonAttributeSchema):
+    typename = "string"
+
+    def __init__(self, length, *args, **kwargs):
+        super(StringSchema, self).__init__(*args, **kwargs)
+        self.length = length
+
+    meta_schema_ext = {
+        voluptuous.Required('length', default=255):
+        voluptuous.All(int, voluptuous.Range(min=1, max=255))
+    }
+
+    @property
+    def schema_ext(self):
+        return voluptuous.All(six.text_type,
+                              voluptuous.Length(max=self.length))
+
+    def jsonify(self):
+        d = super(StringSchema, self).jsonify()
+        d.update({"length": self.length})
+        return d
+
+
+class ResourceTypeAttributes(list):
+    def jsonify(self):
+        d = {}
+        for attr in self:
+            d[attr.name] = attr.jsonify()
+        return d
+
+
+class ResourceTypeSchemaManager(stevedore.ExtensionManager):
+    def __init__(self, *args, **kwargs):
+        super(ResourceTypeSchemaManager, self).__init__(*args, **kwargs)
+        type_schemas = tuple([ext.plugin.meta_schema()
+                              for ext in self.extensions])
+        self._schema = voluptuous.Schema({
+            "name": six.text_type,
+            voluptuous.Required("attributes", default={}): {
+                six.text_type: voluptuous.Any(*tuple(type_schemas))
+            }
+        })
+
+    def __call__(self, definition):
+        return self._schema(definition)
+
+    def attributes_from_dict(self, attributes):
+        return ResourceTypeAttributes(
+            self[attr["type"]].plugin(name=name, **attr)
+            for name, attr in attributes.items())
+
+    def resource_type_from_dict(self, name, attributes):
+        return ResourceType(name, self.attributes_from_dict(attributes))
+
+
+class ResourceType(object):
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+
+    @property
+    def schema(self):
+        schema = {}
+        for attr in self.attributes:
+            schema.update(attr.schema())
+        return schema
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def jsonify(self):
+        return {"name": self.name,
+                "attributes": self.attributes.jsonify()}
