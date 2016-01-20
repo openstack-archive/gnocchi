c14b0e7ecc01b42672ea37b11b23348714e005df
Mehdi Abaakouk <sileht@redhat.com>
2016-01-20 09:45:18 +0100

    Implements resource attribute number
    This change allows to create resource attributes of type number
    
    Blueprint resource-type-rest-api
    Change-Id: I3ada1bc8055fe63253bbf2c329ddbe6437f29d3f

--- gnocchi/resource_type.py
+++ gnocchi/resource_type.py
@@ -12,6 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import numbers
 import re
 import six
 import stevedore
@@ -59,7 +60,10 @@ class CommonAttributeSchema(object):
             voluptuous.Required('type'): cls.typename,
             voluptuous.Required('required', default=True): bool
         }
-        d.update(cls.meta_schema_ext)
+        if callable(cls.meta_schema_ext):
+            d.update(cls.meta_schema_ext())
+        else:
+            d.update(cls.meta_schema_ext)
         return d
 
     def schema(self):
@@ -105,6 +109,33 @@ class StringSchema(CommonAttributeSchema):
 class UUIDSchema(CommonAttributeSchema):
     typename = "uuid"
     schema_ext = staticmethod(utils.UUID)
+
+
+class NumberSchema(CommonAttributeSchema):
+    typename = "number"
+
+    def __init__(self, min, max, *args, **kwargs):
+        super(NumberSchema, self).__init__(*args, **kwargs)
+        self.min = min
+        self.max = max
+
+    meta_schema_ext = {
+        voluptuous.Required('min', default=None): voluptuous.Any(
+            None, voluptuous.All(numbers.Real, voluptuous.Range(min=0))),
+        voluptuous.Required('max', default=None): voluptuous.Any(
+            None, voluptuous.All(numbers.Real, voluptuous.Range(min=0)))
+    }
+
+    @property
+    def schema_ext(self):
+        return voluptuous.All(voluptuous.Any(float, int),
+                              voluptuous.Range(min=self.min,
+                                               max=self.max))
+
+    def jsonify(self):
+        d = super(NumberSchema, self).jsonify()
+        d.update({"min": self.min, "max": self.max})
+        return d
 
 
 class ResourceTypeAttributes(list):
