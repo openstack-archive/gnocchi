4986269208f77dc9bc73627c109a5467d3088c6e
Julien Danjou <julien@danjou.info>
2016-02-04 18:16:21 +0100

    carbonara: serialize AggregatedTimeSerie using RLE, XOR encoding and LZ4
    This changes the serialization format of AggregatedTimeSerie to use a
    double-delta RLE encoding for the timestamps followed by a LZ4
    compression. The values are encoded using an XOR based encoding.
    
    This reduces the storage to a worst case scenario of ~7.3 bytes per
    datapoint, and can compress data down to 0.2 byte per value for simple
    discrete values.
    
    Change-Id: Ie9cc8bf036569db5bfc3213bc16145bc16c64108

--- gnocchi/carbonara.py
+++ gnocchi/carbonara.py
@@ -16,12 +16,16 @@
 # under the License.
 """Time series data manipulation, better with pancetta."""
 
+import datetime
 import functools
 import logging
 import numbers
 import operator
 import re
+import struct
 
+import bitarray
+import lz4
 import msgpack
 import pandas
 import six
@@ -65,6 +69,113 @@ class SerializableMixin(object):
 
     def serialize(self):
         return msgpack.dumps(self.to_dict())
+
+
+def double_to_int(f):
+    """Convert a 64 bits float to a 64 bits integer."""
+    return struct.unpack("!Q", struct.pack("!d", f))[0]
+
+
+def int_to_double(i):
+    """Convert an integer to a 64 bits float."""
+    return struct.unpack("!d", struct.pack("!Q", i))[0]
+
+
+def to_binary64(v):
+    """Convert value to a binary representation on 64 bits as a string."""
+    return "{:064b}".format(v)
+
+
+def count_lead_and_trail_zeroes(d):
+    """Count the number of leading and trailing zeroes in an integer."""
+    as_str = "{:64b}".format(d)
+    try:
+        leading_zeroes = as_str.index("1")
+        trailing_zeroes = 63 - as_str.rindex("1")
+    except ValueError:
+        return 64, 64
+    return leading_zeroes, trailing_zeroes
+
+
+def encode_xor_floats(floats):
+    """Encode a list of floats using XOR encoding.
+
+    Encoding stolen from paper:
+         "Gorilla: A Fast, Scalable, In-Memory Time Series Database"
+    """
+    if len(floats) == 0:
+        return b""
+    prev = double_to_int(floats[0])
+    encoded = bitarray.bitarray(to_binary64(prev))
+    prev_lz = prev_tz = 0
+    for v in floats:
+        v = double_to_int(v)
+        xor = prev ^ v
+        if xor == 0:
+            encoded.append(0)
+        else:
+            encoded.append(1)
+
+            lz, tz = count_lead_and_trail_zeroes(xor)
+
+            if lz >= 32:
+                lz = 31
+
+            if prev_lz != 0 and lz >= prev_lz and tz >= prev_tz:
+                encoded.append(0)
+                encoded.extend(
+                    to_binary64(xor >> prev_tz)[- (64 - prev_lz - prev_tz):])
+            else:
+                prev_lz = lz
+                prev_tz = tz
+                encoded.append(1)
+                encoded.extend(to_binary64(lz)[-5:])
+                sigbits = 64 - lz - tz
+                encoded.extend(to_binary64(sigbits)[-6:])
+                encoded.extend(to_binary64(xor >> tz)[-sigbits:])
+        prev = v
+
+    return encoded.tobytes()
+
+_ARRAY_3_ZERO = bitarray.bitarray('000')
+_ARRAY_2_ZERO = bitarray.bitarray('00')
+
+def decode_xor_floats(encoded, number_of_values):
+    if number_of_values == 0:
+        return []
+
+    array = bitarray.bitarray()
+    array.frombytes(encoded.tobytes())
+
+    results = []
+    # Read first value
+    results.append(struct.unpack("!d", array[:64].tobytes())[0])
+
+    index = 64
+
+    for _ in range(number_of_values - 1):
+        index += 1
+        if array[index - 1]:
+            index += 1
+            if array[index - 1]:
+                bits = _ARRAY_3_ZERO + array[index:index + 5]
+                leading = struct.unpack("!B", bits.tobytes())[0]
+                bits = _ARRAY_2_ZERO + array[index + 5:index + 11]
+                trailing = 64 - leading - struct.unpack(
+                    "!B", bits.tobytes())[0]
+                index += 11
+            sigbits = 64 - leading - trailing
+            bits = bitarray.bitarray(
+                '0' * (64 - sigbits)) + array[index:index + sigbits]
+            num = struct.unpack("!Q", bits.tobytes())[0]
+            index += sigbits
+
+            vbits = float_to_int(results[-1])
+            vbits ^= num << trailing
+            results.append(int_to_float(vbits))
+        else:
+            results.append(results[-1])
+    return results
 
 
 class TimeSerie(SerializableMixin):
@@ -347,21 +458,55 @@ class AggregatedTimeSerie(TimeSerie):
         :param d: The dict.
         :returns: A TimeSerie object
         """
-        timestamps, values = cls._timestamps_and_values_from_dict(d['values'])
+        # FIXME(jd) sampling must be mandatory at this stage!
+        sampling = d.get('sampling')
+        prev_timestamp = pandas.Timestamp(d.get('first_timestamp') * 10e8)
+        timestamps = []
+        for delta in msgpack.loads(lz4.loads(d.get('timestamps'))):
+            prev_timestamp = datetime.timedelta(
+                seconds=delta * sampling) + prev_timestamp
+            timestamps.append(prev_timestamp)
+
+        values = decode_xor_floats(d.get('values'), len(timestamps))
+
         return cls.from_data(
-            timestamps, values,
+            timestamps,
+            values,
             max_size=d.get('max_size'),
             sampling=d.get('sampling'),
             aggregation_method=d.get('aggregation_method', 'mean'))
 
     def to_dict(self):
-        d = super(AggregatedTimeSerie, self).to_dict()
-        d.update({
+        if self.ts.empty:
+            timestamps = []
+            values = []
+            first_timestamp = 0
+        else:
+            first_timestamp = float(
+                self.get_split_key(self.ts.index[0], self.sampling))
+            timestamps = []
+            prev_timestamp = pandas.Timestamp(
+                first_timestamp * 10e8).to_pydatetime()
+            # Use double delta encoding for timestamps
+            for i in self.ts.index:
+                # Convert to pydatetime because it's faster to compute than
+                # Pandas' objects
+                asdt = i.to_pydatetime()
+                timestamps.append(
+                    int((asdt - prev_timestamp).total_seconds()
+                        / self.sampling))
+                prev_timestamp = asdt
+            values = self.ts.values.tolist()
+
+        return {
+            'first_timestamp': first_timestamp,
             'aggregation_method': self.aggregation_method,
             'max_size': self.max_size,
             'sampling': self._serialize_time_period(self._sampling),
-        })
-        return d
+            # LZ4 is more efficient than xor encoding for those values
+            'timestamps': lz4.dumps(msgpack.dumps(timestamps)),
+            'values': encode_xor_floats(values),
+        }
 
     def _truncate(self):
         """Truncate the timeserie."""
