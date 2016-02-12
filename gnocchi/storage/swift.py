9569bbdda8161407e75c4ed1e62e16769d17a2fb
gordon chung <gord@live.ca>
2016-02-12 11:28:58 -0500

    partition unprocessed measures across workers
    this patch assigned each worker to work on a specific partition of
    measures backlog. it uses the metric reporting worker to dynamically
    set the size of each partition.
    
    by default each processing worker will handle a specific block of 128
    metrics. the reporting worker will periodically check backlog size
    and broadcast to all processing workers whether to grab larger chunks
    (if backlog is big) or smaller chunks (when backlog is small).
    
    this minimises overlap between workers. it may result in no-op
    workers if backlog is very small. overlap may occur as the workers
    query backlog at different times and may possibly have different block
    sizes.
    
    a minimum block size is set at 16 metrics and a maximum of 256.
    in theory if 8 workers are set per service, it will grab roughly 128
    to 2048 metrics at a time.
    
    Change-Id: Iad7b6b9a56fefcbe85e343e38c0738c2b33efb92

--- gnocchi/storage/swift.py
+++ gnocchi/storage/swift.py
@@ -105,13 +105,15 @@ class SwiftStorage(_carbonara.CarbonaraBasedStorage):
             data)
 
     def _list_metric_with_measures_to_process(self, full=False):
-        limit = self.METRIC_WITH_MEASURES_TO_PROCESS_BATCH_SIZE
-        if full:
-            limit = None
+        limit = None
+        if not full:
+            limit = self.partition_size * (self.partition + 1)
         headers, files = self.swift.get_container(self.MEASURE_PREFIX,
                                                   delimiter='/',
                                                   full_listing=full,
                                                   limit=limit)
+        if not full:
+            files = files[self.partition_size * self.partition:]
         return set(f['subdir'][:-1] for f in files if 'subdir' in f)
 
     def _list_measure_files_for_metric_id(self, metric_id):
