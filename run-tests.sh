092785b7298c0e20fe4513443e53b68915bdc75a
Julien Danjou <julien@danjou.info>
2016-04-15 16:40:57 +0200

    Add a S3 based storage driver
    This relies on s3rver to test (via pifpaf), but also has been tested
    against AWS S3 and works perfectly.
    
    Change-Id: I99d6394f662fdecd4021990aa3377516bebff674

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,21 @@ do
     export GNOCCHI_TEST_STORAGE_DRIVER=$storage
     for indexer in ${GNOCCHI_TEST_INDEXER_DRIVERS}
     do
-        pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
+        if [ "$storage" = "s3" ]
+        then
+            if ! which s3rver >/dev/null 2>&1
+            then
+                mkdir npm-s3rver
+                export NPM_CONFIG_PREFIX=npm-s3rver
+                npm install s3rver --global
+                export PATH=$PWD/npm-s3rver/bin:$PATH
+            fi
+            pifpaf -e GNOCCHI_STORAGE run s3rver -- \
+                   pifpaf -e GNOCCHI_INDEXER run $indexer -- \
+                   ./tools/pretty_tox.sh $*
+        else
+            pifpaf -e GNOCCHI_INDEXER run $indexer -- \
+                   ./tools/pretty_tox.sh $*
+        fi
     done
 done
