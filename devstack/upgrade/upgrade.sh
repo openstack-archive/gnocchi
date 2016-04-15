f2062f02f406f13382b852c5de049fa1a48647ae
Mehdi Abaakouk <sileht@redhat.com>
2016-04-15 12:03:48 +0200

    Add grenade plugin
    Partial-Fix: #1566445
    Depends-On: I2cb3c75bdd738a8e19796456f0aed14237ef755e
    Change-Id: Ie19eff8761621789d82674c7ee8b4120ecb40239

---
+++ devstack/upgrade/upgrade.sh
@@ -0,0 +1,62 @@
+echo "*********************************************************************"
+echo "Begin $0"
+echo "*********************************************************************"
+
+# Clean up any resources that may be in use
+cleanup() {
+    set +o errexit
+
+    echo "*********************************************************************"
+    echo "ERROR: Abort $0"
+    echo "*********************************************************************"
+
+    # Kill ourselves to signal any calling process
+    trap 2; kill -2 $$
+}
+
+trap cleanup SIGHUP SIGINT SIGTERM
+
+# Keep track of the grenade directory
+RUN_DIR=$(cd $(dirname "$0") && pwd)
+
+# Source params
+source $GRENADE_DIR/grenaderc
+
+# Import common functions
+source $GRENADE_DIR/functions
+
+# This script exits on an error so that errors don't compound and you see
+# only the first error that occurred.
+set -o errexit
+
+# Get Gnocchi functions from devstack plugin
+source $GNOCCHI_DEVSTACK_DIR/settings
+
+# Print the commands being run so that we can see the command that triggers
+# an error.
+set -o xtrace
+
+# Install the target gnocchi
+source $GNOCCHI_DEVSTACK_DIR/plugin.sh stack install
+
+# FIXME(sileht): don't support only master
+# HACK(sileht): since devstack-gate doesn't fully support our branch
+# naming scheme, e store the Gnocchi BASE branch into OVERRIDE_GNOCCHI_PROJECT_BRANCH
+BASE_GNOCCHI_BRANCH=$OVERRIDE_GNOCCHI_PROJECT_BRANCH
+TARGET_GNOCCHI_BRANCH=$TARGET_DEVSTACK_BRANCH
+
+# calls upgrade-gnocchi for specific release
+upgrade_project gnocchi $RUN_DIR $BASE_GNOCCHI_BRANCH $TARGET_GNOCCHI_BRANCH
+
+GNOCCHI_BIN_DIR=$(dirname $(which gnocchi-upgrade))
+$GNOCCHI_BIN_DIR/gnocchi-upgrade || die $LINENO "Gnocchi upgrade"
+
+# Start Gnochi
+start_gnocchi
+
+ensure_services_started  gnocchi-api gnocchi-metricd gnocchi-statsd
+
+set +o xtrace
+echo "*********************************************************************"
+echo "SUCCESS: End $0"
+echo "*********************************************************************"
