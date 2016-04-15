b5904f58a5e7a8903fc486a07f223ffc8ded7249
Mehdi Abaakouk <sileht@redhat.com>
2016-04-15 12:03:48 +0200

    Add grenade plugin
    Partial-Fix: #1566445
    Change-Id: Ie19eff8761621789d82674c7ee8b4120ecb40239

---
+++ devstack/upgrade/shutdown.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#
+
+set -o errexit
+
+source $GRENADE_DIR/grenaderc
+source $GRENADE_DIR/functions
+
+source $BASE_DEVSTACK_DIR/functions
+source $BASE_DEVSTACK_DIR/stackrc # needed for status directory
+source $BASE_DEVSTACK_DIR/lib/tls
+source $BASE_DEVSTACK_DIR/lib/apache
+
+# Locate the Gnocchi plugin and get its functions
+GNOCCHI_DEVSTACK_DIR=$(dirname $(dirname $0))
+source $GNOCCHI_DEVSTACK_DIR/plugin.sh
+
+set -o xtrace
+
+stop_gnocchi
+# NOTE(sileht): old gnocchi metricd doesn't stop correctly
+# TODO(sileht): move this in from-mitaka directory
+pkill -9 -f gnocchi-metricd
+
+# ensure everything is stopped
+ensure_services_stopped gnocchi-api gnocchi-metricd gnocchi-statsd
