f153744ea80ca2e7ae509b01dd704f594c4559c7
Julien Danjou <julien@danjou.info>
2016-05-13 10:03:27 +0200

    sqlalchemy: add retry on deadlock for delete_resource()
    Deleting a resource can trigger a deadlock using PostgreSQL.
    
     oslo_db.exception.DBDeadlock: (psycopg2.extensions.TransactionRollbackError) deadlock detected
     DETAIL:  Process 91956 waits for RowExclusiveLock on relation 21454 of database 12641; blocked by process 91968.
     Process 91968 waits for ShareRowExclusiveLock on relation 21416 of database 12641; blocked by process 91956.
     HINT:  See server log for query details.
      [SQL: 'DELETE FROM resource WHERE resource.id = %(id_1)s'] [parameters: {'id_1': UUID('4b935d09-6aa6-4403-8f8c-a76628bfad60')}]
    
    Change-Id: Ife5c3ae5fdf921f27a6ca964d82e99a4ae45aa0a

--- gnocchi/indexer/sqlalchemy.py
+++ gnocchi/indexer/sqlalchemy.py
@@ -572,6 +572,8 @@ class SQLAlchemyIndexer(indexer.IndexerDriver):
 
         session.expire(r, ['metrics'])
 
+    @oslo_db.api.wrap_db_retry(retry_on_deadlock=True,
+                               max_retries=_TRANSACTION_MAX_RETRY)
     def delete_resource(self, resource_id):
         with self.facade.writer() as session:
             # We are going to delete the resource; the on delete will set the
