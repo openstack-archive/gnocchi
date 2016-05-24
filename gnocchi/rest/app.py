ef78d658eedd4b9e565f1473c4b02f486f187336
Julien Danjou <julien@danjou.info>
2016-05-24 17:16:50 +0200

    Use pbr WSGI script to build gnocchi-api
    Change-Id: I2298f9cb94a684747f4b4dbc262cdcab7de49175

--- gnocchi/rest/app.py
+++ gnocchi/rest/app.py
@@ -22,7 +22,6 @@ from oslo_policy import policy
 from paste import deploy
 import pecan
 import webob.exc
-from werkzeug import serving
 
 from gnocchi import exceptions
 from gnocchi import indexer as gnocchi_indexer
@@ -133,30 +132,11 @@ def _setup_app(root, conf, indexer, storage, not_implemented_middleware):
     return app
 
 
-class WerkzeugApp(object):
-    # NOTE(sileht): The purpose of this class is only to be used
-    # with werkzeug to create the app after the werkzeug
-    # fork gnocchi-api and avoid creation of connection of the
-    # storage/indexer by the main process.
-
-    def __init__(self, conf):
-        self.app = None
-        self.conf = conf
-
-    def __call__(self, environ, start_response):
-        if self.app is None:
-            self.app = load_app(conf=self.conf)
-        return self.app(environ, start_response)
-
-
-def build_server():
-    conf = service.prepare_service()
-    serving.run_simple(conf.api.host, conf.api.port,
-                       WerkzeugApp(conf),
-                       processes=conf.api.workers)
-
-
 def app_factory(global_config, **local_conf):
     global APPCONFIGS
     appconfig = APPCONFIGS.get(global_config.get('configkey'))
     return _setup_app(root=local_conf.get('root'), **appconfig)
+
+
+def build_wsgi_app():
+    return load_app(service.prepare_service())
