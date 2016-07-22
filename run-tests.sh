fd251389b8cb6c980d22ea9d85ac5026d91cbb31
Julien Danjou <julien@danjou.info>
2016-07-22 12:07:59 +0200

    DNM: pifpaf debug
    Change-Id: I1214a14103c2e26d9dd5291887a9225f6c93cce5

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,9 @@ do
     export GNOCCHI_TEST_STORAGE_DRIVER=$storage
     for indexer in ${GNOCCHI_TEST_INDEXER_DRIVERS}
     do
-        pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
+        mysql --version || true
+        mysqld --version || true
+        pifpaf --debug -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
+        dmesg
     done
 done
