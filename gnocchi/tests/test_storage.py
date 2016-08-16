a57c1eca952150b781dffbf5405656a14232dbe4
Julien Danjou <julien@danjou.info>
2016-08-16 14:28:35 +0200

    storage: do not list metrics on each measure processing
    This should simplify and speed-up test run. Also add a specific test to
    check list_metric_with_measures_to_process().
    
    Change-Id: I981f19ec658a4d9928b9170876b3f5782041806b

--- gnocchi/tests/test_storage.py
+++ gnocchi/tests/test_storage.py
@@ -45,11 +45,10 @@ class TestStorageDriver(tests_base.TestCase):
                                          archive_policy_name)
         return m, m_sql
 
-    @staticmethod
-    def trigger_processing(storage, index):
-        metrics = storage.list_metric_with_measures_to_process(
-            None, None, full=True)
-        storage.process_background_tasks(index, metrics, sync=True)
+    def trigger_processing(self, metrics=None):
+        if metrics is None:
+            metrics = [str(self.metric.id)]
+        self.storage.process_background_tasks(self.index, metrics, sync=True)
 
     def test_get_driver(self):
         self.conf.set_override('driver', 'null', 'storage')
@@ -63,7 +62,7 @@ class TestStorageDriver(tests_base.TestCase):
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 13, 0, 1), 1),
@@ -73,33 +72,48 @@ class TestStorageDriver(tests_base.TestCase):
                         side_effect=ValueError("boom!")):
             with mock.patch('gnocchi.carbonara.TimeSerie.unserialize',
                             side_effect=ValueError("boom!")):
-                self.trigger_processing(self.storage, self.index)
+                self.trigger_processing()
 
         m = self.storage.get_measures(self.metric)
         self.assertIn((utils.datetime_utc(2014, 1, 1), 86400.0, 1), m)
         self.assertIn((utils.datetime_utc(2014, 1, 1, 13), 3600.0, 1), m)
         self.assertIn((utils.datetime_utc(2014, 1, 1, 13), 300.0, 1), m)
 
+    def test_list_metric_with_measures_to_process(self):
+        metrics = self.storage.list_metric_with_measures_to_process(
+            None, None, full=True)
+        self.assertEqual([], metrics)
+        self.storage.add_measures(self.metric, [
+            storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
+        ])
+        metrics = self.storage.list_metric_with_measures_to_process(
+            None, None, full=True)
+        self.assertEqual([str(self.metric.id)], metrics)
+        self.trigger_processing()
+        metrics = self.storage.list_metric_with_measures_to_process(
+            None, None, full=True)
+        self.assertEqual([], metrics)
+
     def test_delete_nonempty_metric(self):
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
         self.storage.delete_metric(self.metric)
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
     def test_delete_nonempty_metric_unprocessed(self):
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
         ])
         self.storage.delete_metric(self.metric)
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
     def test_delete_expunge_metric(self):
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
         self.index.delete_metric(self.metric.id)
         self.storage.expunge_metrics(self.index, sync=True)
         self.assertRaises(indexer.NoSuchMetric, self.index.delete_metric,
@@ -125,7 +139,7 @@ class TestStorageDriver(tests_base.TestCase):
         self.storage.add_measures(m, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, i, j), 100)
             for i in six.moves.range(0, 60) for j in six.moves.range(0, 60)])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(m.id)])
 
         self.assertEqual(3661, len(self.storage.get_measures(m)))
 
@@ -136,7 +150,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 6, i, j, 0), 100)
             for i in six.moves.range(2) for j in six.moves.range(0, 60, 2)]
         self.storage.add_measures(m, measures)
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(m.id)])
 
         # add measure to end, in same aggregate time as last point.
         self.storage.add_measures(m, [
@@ -144,7 +158,7 @@ class TestStorageDriver(tests_base.TestCase):
 
         with mock.patch.object(self.storage, '_store_metric_measures') as c:
             # should only resample last aggregate
-            self.trigger_processing(self.storage, self.index)
+            self.trigger_processing([str(m.id)])
         count = 0
         for call in c.mock_calls:
             # policy is 60 points and split is 48. should only update 2nd half
@@ -159,14 +173,14 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 6, i, j, 0), 100)
             for i in six.moves.range(2) for j in six.moves.range(0, 60, 2)]
         self.storage.add_measures(m, measures)
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(m.id)])
 
         # add measure to end, in same aggregate time as last point.
         new_point = datetime.datetime(2014, 1, 6, 1, 58, 1)
         self.storage.add_measures(m, [storage.Measure(new_point, 100)])
 
         with mock.patch.object(self.storage, '_add_measures') as c:
-            self.trigger_processing(self.storage, self.index)
+            self.trigger_processing([str(m.id)])
         for __, args, __ in c.mock_calls:
             self.assertEqual(
                 args[3].first, carbonara.TimeSerie.round_timestamp(
@@ -179,7 +193,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 9, 31), 4),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 12, 45), 44),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1), 86400.0, 39.75),
@@ -193,7 +207,7 @@ class TestStorageDriver(tests_base.TestCase):
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2015, 1, 1, 12, 0, 1), 69),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1), 86400.0, 39.75),
@@ -207,13 +221,13 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 1), 69),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 7, 31), 42),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.storage.add_measures(self.metric, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 9, 31), 4),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 12, 45), 44),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1), 86400.0, 39.75),
@@ -246,7 +260,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 9, 31), 4),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 12, 45), 44),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing()
 
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1), 86400.0, 39.75),
@@ -414,7 +428,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 10, 31), 4),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 13, 10), 4),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(self.metric.id), str(metric2.id)])
 
         values = self.storage.get_cross_metric_measures([self.metric, metric2])
         self.assertEqual([
@@ -500,7 +514,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 9, 31), 6),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 13, 10), 2),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(self.metric.id), str(metric2.id)])
 
         values = self.storage.get_cross_metric_measures([self.metric, metric2])
         self.assertEqual([
@@ -527,7 +541,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 9, 31), 6),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 13, 10), 2),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(self.metric.id), str(metric2.id)])
 
         self.assertEqual(
             {metric2: [],
@@ -560,7 +574,7 @@ class TestStorageDriver(tests_base.TestCase):
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 5), 1),
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 10), 1),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(m.id)])
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1, 12, 0, 0), 5.0, 1.0),
             (utils.datetime_utc(2014, 1, 1, 12, 0, 5), 5.0, 1.0),
@@ -573,7 +587,7 @@ class TestStorageDriver(tests_base.TestCase):
         self.storage.add_measures(m, [
             storage.Measure(datetime.datetime(2014, 1, 1, 12, 0, 15), 1),
         ])
-        self.trigger_processing(self.storage, self.index)
+        self.trigger_processing([str(m.id)])
         self.assertEqual([
             (utils.datetime_utc(2014, 1, 1, 12, 0, 0), 5.0, 1.0),
             (utils.datetime_utc(2014, 1, 1, 12, 0, 5), 5.0, 1.0),
