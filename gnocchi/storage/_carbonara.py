9cbaee5d035c55755f4cd60f9360230e181d9589
Julien Danjou <julien@danjou.info>
2016-12-06 18:13:13 +0100

    carbonara: log a message and do not fail if a file is missing
    When rewriting data for compression, it's possible a file
    <metric>_<splitkey>_<aggregation_method>_<granularity> is missing,
    because of some data corruption. In that case the following traceback happens:
    
      Traceback (most recent call last):
        File "/usr/lib/python2.7/site-packages/gnocchi/storage/_carbonara.py", line 557, in process_new_measures
          ignore_too_old_timestamps=True)
        File "/usr/lib/python2.7/site-packages/gnocchi/carbonara.py", line 217, in set_values
          before_truncate_callback(self)
        File "/usr/lib/python2.7/site-packages/gnocchi/storage/_carbonara.py", line 551, in _map_add_measures
          for aggregation in agg_methods))
        File "/usr/lib/python2.7/site-packages/gnocchi/storage/_carbonara.py", line 675, in _map_no_thread
          return list(itertools.starmap(method, list_of_args))
        File "/usr/lib/python2.7/site-packages/gnocchi/storage/_carbonara.py", line 316, in _add_measures
          oldest_mutable_timestamp)
        File "/usr/lib/python2.7/site-packages/gnocchi/storage/_carbonara.py", line 249, in _store_timeserie_split
          offset, data = split.serialize(key, compressed=write_full)
      AttributeError: 'NoneType' object has no attribute 'serialize'
    
    This patch makes the driver log a warning and return, so it just ignores
    the failure and continues anyway.
    
    Change-Id: I4f367b2418c8be0067746c88bcce74ca756acf4e

--- gnocchi/storage/_carbonara.py
+++ gnocchi/storage/_carbonara.py
@@ -246,6 +246,20 @@ class CarbonaraBasedStorage(storage.StorageDriver):
                     else:
                         split.merge(existing)
 
+        # if split is None:
+        #     # `split' can be none if existing is None and no split was passed
+        #     # in order to rewrite and compress the data; in that case, it means
+        #     # the split key is present and listed, but some aggregation method
+        #     # or granularity is missing. That means data is corrupted, but it
+        #     # does not mean we have to fail, we can just do nothing and log a
+        #     # warning.
+        #     LOG.warning("No data found for metric %s, granularity %f "
+        #                 "and aggregation method %s (split key %s): "
+        #                 "possible data corruption",
+        #                 metric, archive_policy_def.granularity,
+        #                 aggregation, key)
+        #     return
+
         offset, data = split.serialize(key, compressed=write_full)
 
         return self._store_metric_measures(
