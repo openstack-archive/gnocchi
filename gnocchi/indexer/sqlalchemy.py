f86c26494f58b95b7a192a4aa6d426ea841c3eef
Julien Danjou <julien@danjou.info>
2016-12-19 18:06:41 +0100

    rest,indexer: handle ResourceUUID conversion in the REST API
    This avoids leaking the conversion into the indexer entirely.
    
    Change-Id: Ifbbacfea9bd8d625b1f5a10aeaec4b41342949a3

--- gnocchi/indexer/sqlalchemy.py
+++ gnocchi/indexer/sqlalchemy.py
@@ -1125,7 +1125,6 @@ class QueryTransformer(object):
 
     converters = (
         (base.TimestampUTC, utils.to_datetime),
-        (sqlalchemy_utils.UUIDType, utils.ResourceUUID),
         (types.String, six.text_type),
         (types.Integer, int),
         (types.Numeric, float),
