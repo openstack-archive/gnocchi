dfc20b116fbe38bcfb4edd1e0420cb299f5d2c66
gord chung <gord@live.ca>
2017-03-03 19:31:20 +0000

    push incoming into different sacks
    having everything in one giant folder/bucket/conatiner/object is bad
    because:
    - does not allow for good distribution of backend driver.
    - makes it hella hard to cleanly split work across multiple metricd
    - starves metrics from being processed.
    
    this creates 4096 sacks to distributes them across agents. everything
    else pretty much works the same. the agent will loop through all its
    sacks based on processing_interval. s3 is set to 4 buckets because it
    seems to have a bunch of restrictions.
    
    the number of sacks is constant because it's not safe without more
    code which hasn't been agreed upon.
    
    i used sacks to avoid not naming same as driver paradigms.
    
    Change-Id: Icc32d918fe55416385122470c47d60ddbb30dd34

--- gnocchi/storage/incoming/redis.py
+++ gnocchi/storage/incoming/redis.py
@@ -27,14 +27,12 @@ from gnocchi.storage.incoming import _carbonara
 
 class RedisStorage(_carbonara.CarbonaraBasedStorage):
 
-    STORAGE_PREFIX = "incoming"
-
     def __init__(self, conf):
         super(RedisStorage, self).__init__(conf)
         self._client = redis.get_client(conf)
 
-    def _build_measure_path(self, metric_id, random_id=None):
-        path = os.path.join(self.STORAGE_PREFIX, six.text_type(metric_id))
+    def _build_measure_path(self, sack, metric_id, random_id=None):
+        path = os.path.join(self.SACK_PREFIX % sack, six.text_type(metric_id))
         if random_id:
             if random_id is True:
                 now = datetime.datetime.utcnow().strftime("_%Y%m%d_%H:%M:%S")
@@ -43,38 +41,39 @@ class RedisStorage(_carbonara.CarbonaraBasedStorage):
         return path
 
     def _store_new_measures(self, metric, data):
-        path = self._build_measure_path(metric.id, True)
+        sack = self.compute_sack(metric.id)
+        path = self._build_measure_path(sack, metric.id, True)
         self._client.set(path.encode("utf8"), data)
 
     def _build_report(self, details):
-        match = os.path.join(self.STORAGE_PREFIX, "*")
         metric_details = collections.defaultdict(int)
-        for key in self._client.scan_iter(match=match.encode('utf8')):
-            metric = key.decode('utf8').split(os.path.sep)[1]
-            metric_details[metric] += 1
+        for i in six.moves.range(self.NUM_SACKS):
+            match = os.path.join(self.SACK_PREFIX % i, "*")
+            for key in self._client.scan_iter(match=match.encode('utf8')):
+                metric = key.decode('utf8').split(os.path.sep)[1]
+                metric_details[metric] += 1
         return (len(metric_details.keys()), sum(metric_details.values()),
                 metric_details if details else None)
 
-    def list_metric_with_measures_to_process(self, size, part, full=False):
-        match = os.path.join(self.STORAGE_PREFIX, "*")
+    def list_metric_with_measures_to_process(self, sack):
+        match = os.path.join(self.SACK_PREFIX % sack, "*")
         keys = self._client.scan_iter(match=match.encode('utf8'))
-        measures = set([k.decode('utf8').split(os.path.sep)[1] for k in keys])
-        if full:
-            return measures
-        return set(list(measures)[size * part:size * (part + 1)])
+        return set([k.decode('utf8').split(os.path.sep)[1] for k in keys])
 
-    def _list_measures_container_for_metric_id(self, metric_id):
-        match = os.path.join(self._build_measure_path(metric_id), "*")
+    def _list_measures_container_for_metric_id(self, sack, metric_id):
+        match = os.path.join(self._build_measure_path(sack, metric_id), "*")
         return list(self._client.scan_iter(match=match.encode("utf8")))
 
-    def delete_unprocessed_measures_for_metric_id(self, metric_id):
-        keys = self._list_measures_container_for_metric_id(metric_id)
+    def delete_unprocessed_measures_for_metric(self, metric):
+        sack = self.compute_sack(metric.id)
+        keys = self._list_measures_container_for_metric_id(sack, metric.id)
         if keys:
             self._client.delete(*keys)
 
     @contextlib.contextmanager
     def process_measure_for_metric(self, metric):
-        keys = self._list_measures_container_for_metric_id(metric.id)
+        sack = self.compute_sack(metric.id)
+        keys = self._list_measures_container_for_metric_id(sack, metric.id)
         measures = []
         for k in keys:
             data = self._client.get(k)
