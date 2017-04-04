9776cc99fb7df6dfd2af022b241d9d524c25d34e
gord chung <gord@live.ca>
2017-04-04 15:54:14 +0000

    redis: push to multiple sacks
    enable redis to push to multiple sacks and handle them accordingly
    
    Change-Id: I84c046b80fa46d172dfee69dc4987e8b5a860a1f

--- gnocchi/tests/base.py
+++ gnocchi/tests/base.py
@@ -317,7 +317,7 @@ class TestCase(base.BaseTestCase):
         if self.conf.storage.driver == 'redis':
             # Create one prefix per test
             self.storage.STORAGE_PREFIX = str(uuid.uuid4())
-            self.storage.incoming.STORAGE_PREFIX = str(uuid.uuid4())
+            self.storage.incoming.SACK_PREFIX = str(uuid.uuid4()) + "-%s"
 
         self.storage.upgrade(self.index)
 
