4a59eeefc300c2719c95b0a348e874119f8eb14d
gord chung <gord@live.ca>
2017-04-20 21:14:14 +0000

    configurable sacks framework
    this adds framework to configure sack size without corruption.
    
    - default to 128 sacks
    - add note on how to calculate how many sacks to set
    
    actual ability to change value done in subsequent patch.
    
    Change-Id: I389c1a7ca9b3fe39b3716782e85073796ad26333

--- gnocchi/tests/base.py
+++ gnocchi/tests/base.py
@@ -325,8 +325,7 @@ class TestCase(base.BaseTestCase):
         if self.conf.storage.driver == 'redis':
             # Create one prefix per test
             self.storage.STORAGE_PREFIX = str(uuid.uuid4())
-            self.storage.incoming.SACK_PREFIX = (
-                str(uuid.uuid4()) + self.storage.incoming.SACK_PREFIX)
+            self.storage.incoming.SACK_PREFIX = str(uuid.uuid4())
 
         self.storage.upgrade(self.index)
 
